d9673403cfd64f97cadb3d7a74511cd7f422db52
Daniel Huang <danielhuang@g.ucla.edu>
2019-05-13 11:33:43 -0700

    styled drawer close #9
    Signed-off-by: Daniel Huang <danielhuang@g.ucla.edu>

--- omou-frontend/src/components/Navigation/Navigation.js
+++ omou-frontend/src/components/Navigation/Navigation.js
@@ -6,22 +6,32 @@ import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 import {NavLink} from "react-router-dom";
 
+
 //Material UI Imports
 //TODO: import each component individually (i.e. '@material-ui/core/AppBar') to reduce bundle size
 import {AppBar, Drawer, IconButton, Toolbar, Typography} from "@material-ui/core";
-import MenuIcon from '@material-ui/icons/Menu'
+import MenuIcon from '@material-ui/icons/Menu';
+import MailIcon from '@material-ui/icons/Mail';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import EventIcon from '@material-ui/icons/Event';
+import CourseIcon from '@material-ui/icons/School'
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import AttendanceIcon from '@material-ui/icons/ViewList';
+import ClassIcon from '@material-ui/icons/Class';
+import DirectoryIcon from '@material-ui/icons/Contacts';
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import List from "@material-ui/core/List";
 import Hidden from "@material-ui/core/Hidden";
 import Routes from '../Routes/rootRoutes'
 import CustomTheme from "../../theme/muiTheme"
+import ListItemIcon from "@material-ui/core/ListItemIcon";
 
 //Local Component Imports
 import './Navigation.scss'
 import {MuiThemeProviderOld} from "@material-ui/core/es/styles/MuiThemeProvider";
 import MuiThemeProvider from "@material-ui/core/es/styles/MuiThemeProvider";
-import Grid from "@material-ui/core/Grid";
+
 
 
 class Navigation extends Component {
@@ -35,24 +45,22 @@ class Navigation extends Component {
     };
     render(){
         let NavList = [
-            { name: 'Dashboard', link: '/'},
-            { name: 'Scheduler', link: '/scheduler'},
-            { name: 'Courses', link: '/courses'},
-            { name: 'Registration', link: '/registration'},
-            { name: 'Attendance', link: '/attendance'},
-            { name: 'Gradebook', link: '/gradebook'},
-            { name: 'Directory', link: '/directory'},
+            { name: 'Dashboard', link: '/', icon: <DashboardIcon/> },
+            { name: 'Scheduler', link: '/scheduler', icon: <EventIcon/> },
+            { name: 'Courses', link: '/courses', icon: <CourseIcon/>},
+            { name: 'Registration', link: '/registration', icon: <AssignmentIcon/>},
+            { name: 'Attendance', link: '/attendance', icon: <AttendanceIcon/>},
+            { name: 'Gradebook', link: '/gradebook', icon:<ClassIcon/>},
+            { name: 'Directory', link: '/directory', icon:<DirectoryIcon/>},
         ];
 
         const drawer = (
             <div className="DrawerList">
-                <div
-                    // className={classes.toolbar}
-                />
-                <List>
+                <List className={"list"}>
                     {NavList.map((NavItem, index) => (
-                        <ListItem button key={index} component={NavLink} to={NavItem.link}>
-                            <ListItemText primary={NavItem.name} />
+                        <ListItem button key={index} component={NavLink} exact={NavItem.name === "Dashboard"} to={NavItem.link} className={"listItem"}>
+                            <ListItemIcon>{NavItem.icon}</ListItemIcon>
+                            <ListItemText primary={NavItem.name} className={"text"}/>
                         </ListItem>
                     ))}
                 </List>
@@ -76,8 +84,8 @@ class Navigation extends Component {
                                     <MenuIcon/>
                                 </IconButton>
                             </Hidden>
-                            <Typography component={NavLink} to="/" variant="title" color="inherit">
-                                Omou
+                            <Typography component={NavLink} to="/" variant="title" color="#29abd6">
+                                omou
                             </Typography>
                         </Toolbar>
                     </AppBar>
@@ -109,9 +117,6 @@ class Navigation extends Component {
                     </nav>
                     <main className="OmouMain">
                         <Routes/>
-                        {/*<Grid container spacing={16}>*/}
-                            {/**/}
-                        {/*</Grid>*/}
                     </main>
 
                 </div>
