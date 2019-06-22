4ff88b0a58e41e90b9373d611b684230d20c9ad5
Jeffrey Su <jsflca2012@gmail.com>
2019-06-22 12:21:47 -0700

    commented first commit
    commented first commit

--- omou-frontend/src/index.js
+++ omou-frontend/src/index.js
@@ -18,6 +18,7 @@ ReactDOM.render(
     document.getElementById('root')
 );
 //tedt
+//test2
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
