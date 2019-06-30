b9639b3deaaf031d7eb55b5e2e3d91825eff4d19
Neil Allavarpu <allavarpuneil@gmail.com>
2019-06-30 10:50:46 -0700

    Combine register course and tutor into one dropdown
    Close #48
    
    Registering for a new course and tutor are now combined into one dropdown with an option for either choice.

--- omou-frontend/src/components/FeatureViews/Registration/RegistrationActions.js
+++ omou-frontend/src/components/FeatureViews/Registration/RegistrationActions.js
@@ -1,79 +1,118 @@
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import PropTypes from "prop-types";
+import React, {useState} from "react";
 
-//Material UI Imports
+// Material UI Imports
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import NewUser from "@material-ui/icons/PersonAdd";
 import NewTutor from "@material-ui/icons/Group";
 import NewCourse from "@material-ui/icons/School";
-import { NavLink } from "react-router-dom";
-import './registration.scss';
+import {NavLink} from "react-router-dom";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import {withStyles} from "@material-ui/core/styles";
+import "./registration.scss";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
 
-class RegistrationActions extends Component {
-    constructor(props) {
-        super(props);
+const StyledMenu = withStyles({
+    paper: {
+        border: "1px solid #d3d4d5",
+    },
+})((props) => (
+    <Menu
+        elevation={0}
+        getContentAnchorEl={null}
+        anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center",
+        }}
+        transformOrigin={{
+            vertical: "top",
+            horizontal: "center",
+        }}
+        {...props}
+    />
+));
+
+const StyledMenuItem = withStyles((theme) => ({
+    root: {
+        "&:focus": {
+            backgroundColor: theme.palette.primary.main,
+            "& .MuiListItemIcon-root, & .MuiListItemText-primary": {
+                color: theme.palette.common.white,
+            },
+        },
+    },
+}))(MenuItem);
+
+function RegistrationActions(props) {
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    function handleClick(event) {
+        setAnchorEl(event.currentTarget);
     }
 
-    render() {
-        let courseRoute = "";
-        (this.props.courseTitle !== undefined && this.props.courseTitle !== "") ? courseRoute = '/' + encodeURIComponent(this.props.courseTitle) : courseRoute="";
-        return (<Grid container
-            direction={"row"}
-            justify={"flex-start"}
-            className={"registration-action-control"}>
+    function handleClose() {
+        setAnchorEl(null);
+    }
+
+    const courseRoute = props.courseTitle ? encodeURIComponent(this.props.courseTitle) : "";
+    return (
+        <Grid container
+            direction="row"
+            justify="flex-start"
+            className="registration-action-control">
             <Grid item>
-                <Button component={NavLink} to={'/registration/form/student'}
+                <Button component={NavLink} to="/registration/form/student"
                     variant="outlined"
                     color="secondary"
-                    className={"button"}>
-                    <NewUser className={"icon"} />
+                    className="button">
+                    <NewUser className="icon" />
                     New Student
-                                </Button>
+                </Button>
             </Grid>
             <Grid item>
-                <Button component={NavLink} to={`/registration/form/tutoring${courseRoute}`}
+                <Button
                     variant="outlined"
                     color="secondary"
-                    className={"button"}>
+                    className="button"
+                    ria-controls="simple-menu"
+                    aria-haspopup="true"
+                    onClick={handleClick}>
                     <NewTutor className={"icon"} />
-                    New Tutoring
-                                </Button>
+                    Register
+                </Button>
+                <StyledMenu
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}>
+                    <StyledMenuItem
+                        component={NavLink}
+                        to={`/registration/form/course/${courseRoute}`}>
+                        <ListItemIcon>
+                            <NewCourse className="icon" />
+                        </ListItemIcon>
+                        <ListItemText primary="Course" />
+                    </StyledMenuItem>
+                    <StyledMenuItem
+                        component={NavLink}
+                        to={`/registration/form/tutoring/${courseRoute}`}>
+                        <ListItemIcon>
+                            <NewTutor className="icon" />
+                        </ListItemIcon>
+                        <ListItemText primary="Tutoring" />
+                    </StyledMenuItem>
+                </StyledMenu>
             </Grid>
-            <Grid item>
-                <Button component={NavLink} to={`/registration/form/course${courseRoute}`}
-                    variant="outlined"
-                    color="secondary"
-                    className={"button"}>
-                    <NewCourse className={"icon"} />
-                    New Course
-                                </Button>
-            </Grid>
-        </Grid>)
-    }
+        </Grid>
+    );
 }
 
 RegistrationActions.propTypes = {
-    stuffActions: PropTypes.object,
-    stuffs: PropTypes.array,
-    course_title: PropTypes.string,
+    courseTitle: PropTypes.string,
     admin: PropTypes.bool,
-}
+};
 
-function mapStateToProps(state) {
-    return {
-        stuffs: state.stuff
-    };
-}
-
-function mapDispatchToProps(dispatch) {
-    return {
-    };
-}
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(RegistrationActions);
+export default RegistrationActions;
