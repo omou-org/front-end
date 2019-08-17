c0ae825316ed2d26bca92b65a08192ef51f6ea81
Daniel Huang <danielhuang@g.ucla.edu>
2019-08-17 13:50:20 -0700

    break real quick
    Signed-off-by: Daniel Huang <danielhuang@g.ucla.edu>

--- omou-frontend/src/components/FeatureViews/Accounts/TabComponents/CourseViewer.js
+++ omou-frontend/src/components/FeatureViews/Accounts/TabComponents/CourseViewer.js
@@ -22,43 +22,74 @@ class CourseViewer extends Component {
     }
 
     componentWillMount() {
-        console.log(this.props);
         this.setState({
             current: this.props.current,
             userRole: this.props.user_role,
             userID: this.props.userID,
+            userEnrollments: this.props.enrollments[this.props.user_id],
         });
-    }
-    shouldComponentUpdate(nextProps, nextState, nextContext) {
-
     }
 
     setCourses = () => {
         let today = new Date();
-        let filterCourseByDate = (endDate)=>{
+        let filterCourseByDate = (endDate) => {
             let inputEndDate = new Date(endDate);
             // if we're rendering current courses then check if the end date for the course is later than today
             // else if we're rendering past courses, then check if the end date for the course is later than today
-            return ( this.props.current ? inputEndDate > today : inputEndDate < today)
+            return (this.props.current ? inputEndDate > today : inputEndDate < today)
         };
         let userCourseList;
-        switch(this.props.user_role){
+        switch (this.props.user_role) {
             case "instructor":
                 // only keep course if instructor is associated with the course
-                userCourseList = Object.keys(this.props.courses).filter((courseID)=>{
+                userCourseList = Object.keys(this.props.courses).filter((courseID) => {
                     let course = this.props.courses[courseID];
                     return filterCourseByDate(course.schedule.end_date) && course.instructor_id === this.props.user_id;
                 });
                 break;
             case "student":
                 // only keep course if the student is on the roster
-                userCourseList = Object.keys(this.props.courses).filter((courseID)=>{
+                userCourseList = Object.keys(this.props.courses).filter((courseID) => {
                     let course = this.props.courses[courseID];
                     return filterCourseByDate(course.schedule.end_date) && course.roster.includes(this.props.user_id);
                 });
         }
         return userCourseList;
     };
+
+    courseDataParser(course) {
+        let DayConverter = {
+            1: "Monday",
+            2: "Tuesday",
+            3: "Wednesday",
+            4: "Thursday",
+            5: "Friday",
+            6: "Saturday",
+        };
+        let Days = course.schedule.days.map((day) => {
+            return DayConverter[day];
+        });
+
+        let timeOptions = {hour: "2-digit", minute: "2-digit"};
+        let dateOptions = {year: "numeric", month: "short", day: "numeric"};
+        let startDate = new Date(course.schedule.start_date + course.schedule.start_time),
+            endDate = new Date(course.schedule.end_date + course.schedule.end_time),
+            startTime = startDate.toLocaleTimeString("en-US", timeOptions),
+            endTime = endDate.toLocaleTimeString("en-US", timeOptions);
+        startDate = startDate.toLocaleDateString("en-US", dateOptions);
+        endDate = endDate.toLocaleDateString("en-US", dateOptions);
+        return {
+            Days:Days,
+            startTime: startTime,
+            endTime: endTime,
+            startDate: startDate,
+            endDate: endDate,
+        }
+    }
+
+    numPaidCourses(courseID){
+        let courseEnrollment = this.props.enrollments[this.state.userID][courseID];
+    }
 
     render() {
         this.setCourses();
@@ -93,29 +124,10 @@ class CourseViewer extends Component {
                 </Grid>
             </Grid>
             <Grid container spacing={8}>
-                { this.setCourses().length !== 0 ?
+                {this.setCourses().length !== 0 ?
                     this.setCourses().map((courseID) => {
                         let course = this.props.courses[courseID];
-                        let DayConverter = {
-                            1: "Monday",
-                            2: "Tuesday",
-                            3: "Wednesday",
-                            4: "Thursday",
-                            5: "Friday",
-                            6: "Saturday",
-                        };
-                        let Days = course.schedule.days.map((day) => {
-                            return DayConverter[day];
-                        });
-
-                        let timeOptions = {hour: "2-digit", minute: "2-digit"};
-                        let dateOptions = {year: "numeric", month: "short", day: "numeric"};
-                        let startDate = new Date(course.schedule.start_date + course.schedule.start_time),
-                            endDate = new Date(course.schedule.end_date + course.schedule.end_time),
-                            startTime = startDate.toLocaleTimeString("en-US", timeOptions),
-                            endTime = endDate.toLocaleTimeString("en-US", timeOptions);
-                        startDate = startDate.toLocaleDateString("en-US", dateOptions);
-                        endDate = endDate.toLocaleDateString("en-US", dateOptions);
+                        let {Days, startDate, endDate, startTime, endTime} = this.courseDataParser(course);
                         return (<Grid item md={12} className={'accounts-table-row'}>
                             <Paper square={true}>
                                 <Grid container>
@@ -148,7 +160,7 @@ class CourseViewer extends Component {
                     }) :
                     <Grid item md={12}>
                         <Paper className={'info'}>
-                            <Typography style={{fontWeight:700}}>
+                            <Typography style={{fontWeight: 700}}>
                                 No Courses Yet!
                             </Typography>
                         </Paper>
@@ -166,6 +178,7 @@ function mapStateToProps(state) {
     return {
         usersList: state.Users,
         courses: state.Course.NewCourseList,
+        enrollments: state.Enrollments,
     };
 }
 
