8db5576fa3ee7783e45b54888e21f0a2646a2fd7
Jeffrey Su <jsflca2012@gmail.com>
2019-08-17 13:57:20 -0700

    merge conflict

--- omou-frontend/src/components/FeatureViews/Accounts/UserProfile.js
+++ omou-frontend/src/components/FeatureViews/Accounts/UserProfile.js
@@ -89,6 +89,7 @@ class UserProfile extends Component {
             default:
                 user = -1;
         }
+        
         // this.setState({ ...CourseInView });
         this.setState({
             user: user,
@@ -101,24 +102,7 @@ class UserProfile extends Component {
         this.setState({ value: newTabIndex });
     }
 
-    stringToColor(string) {
-        let hash = 0;
-        let i;
-
-        /* eslint-disable no-bitwise */
-        for (i = 0; i < string.length; i += 1) {
-            hash = string.charCodeAt(i) + ((hash << 5) - hash);
-        }
-
-        let colour = '#';
-
-        for (i = 0; i < 3; i += 1) {
-            const value = (hash >> (i * 8)) & 0xff;
-            colour += `00${value.toString(16)}`.substr(-2);
-        }
-        /* eslint-enable no-bitwise */
-
-        return colour;
+    
     }
 
     render() {
