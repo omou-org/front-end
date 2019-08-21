8a09bf9473412a9366e23ffeaeb19df0d4e75752
Jeffrey Su <jsflca2012@gmail.com>
2019-08-21 15:43:38 -0700

    fixed typography error

--- omou-frontend/src/components/FeatureViews/Accounts/TabComponents/ParentContact.js
+++ omou-frontend/src/components/FeatureViews/Accounts/TabComponents/ParentContact.js
@@ -44,8 +44,7 @@ class ParentContact extends Component {
                             <div className={"actions"} align="right">
                                 <EditIcon />
                             </div>
-                            <Typography className={"body"} align={'left'}>
-                                <Grid container spacing={16} className="bodyText">
+                            <Grid container spacing={16} className="bodyText">
                                     <Grid item xs={5} align="left" className="bold">
                                         Relation
                                     </Grid>
@@ -66,7 +65,6 @@ class ParentContact extends Component {
                                     </Grid>
 
                                 </Grid>
-                            </Typography>
                         </Paper>
                     </Grid>
                 </Grid>
