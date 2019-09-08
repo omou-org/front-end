7921a63080154942134b11d4fc49bc65504e6d21
Jeffrey Su <jsflca2012@gmail.com>
2019-09-08 15:24:21 -0700

    fixed excess backbutton component

--- omou-frontend/src/components/ErrorNotFoundPage/ErrorNotFoundPage.js
+++ omou-frontend/src/components/ErrorNotFoundPage/ErrorNotFoundPage.js
@@ -1,15 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Route, Redirect } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { Card, Paper, Typography } from "@material-ui/core";
 import './ErrorNotFoundPage.scss';
 import BackButton from "../BackButton";
 import Button from "@material-ui/core/Button";
-import BackButton2 from "./modifiedBackButton";
 
 
-function ErrorNotFoundPage() {
+function ErrorNotFoundPage(props) {
     return (
         <div>
             <Paper className={'paper'}>
@@ -19,9 +19,14 @@ function ErrorNotFoundPage() {
                     404.
                     <br />
                     page not found.
-                    <div className="space"/>
-                    <BackButton2 className="space"/>
-                    </Typography>
+                    <div className="space" />
+                    <Button className="backButton"
+                        onClick={
+                            props.history.goBack}
+                    >
+                        <div className="buttonText">Let's go back.</div>
+                    </Button>
+                </Typography>
             </Paper>
         </div>
     );
@@ -30,6 +35,15 @@ function ErrorNotFoundPage() {
 const mapStateToProps = (state) => ({
     auth: state.auth,
 });
+
+ErrorNotFoundPage.propTypes = {
+    history: PropTypes.shape({
+        goBack: PropTypes.func.isRequired,
+    }).isRequired,
+    onBack: PropTypes.func,
+    alertMessage: PropTypes.string,
+};
+
 
 const mapDispatchToProps = () => ({});
 
