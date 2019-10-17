e00af4f3144bfd6f11fd745c0f02829624bb69c5
Jeffrey Su <jsflca2012@gmail.com>
2019-10-16 18:06:46 -0700

    added add notes feature

--- src/components/FeatureViews/Accounts/TabComponents/Notes.js
+++ src/components/FeatureViews/Accounts/TabComponents/Notes.js
@@ -6,58 +6,150 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import "./TabComponents.scss";
 import Paper from "@material-ui/core/Paper";
+import Modal from "@material-ui/core/Modal";
+import Button from "@material-ui/core/Button";
 import EditIcon from "@material-ui/icons/EditOutlined";
-import RemoveIcon from "@material-ui/icons/DeleteForeverOutlined";
-import AlertIcon from "@material-ui/icons/AddAlertOutlined";
+import DoneIcon from "@material-ui/icons/CheckCircleOutlined";
+import AddIcon from "@material-ui/icons/AddOutlined";
+import InputBase from '@material-ui/core/InputBase';
+import TextField from '@material-ui/core/TextField';
 
 class Notes extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            notes:[],
+            notes: [],
+            alert: false,
+            noteBody:"",
         };
     }
 
-    componentWillMount(){
+    componentWillMount() {
         //TODO: notes in descending order
-        this.setState(()=>{
-            let notes = Object.keys(this.props.user_notes).map((noteID)=>{
-               return this.props.user_notes[noteID];
+        this.setState(() => {
+            let notes = Object.keys(this.props.user_notes).map((noteID) => {
+                return this.props.user_notes[noteID];
             });
             return {
-                notes:notes,
+                notes: notes,
             }
         });
     }
 
+    handleClick(notebody) {
+        this.setState({
+            alert: true,
+            noteBody: notebody,
+        })
+    }
+
+    hideWarning() {
+        this.setState({
+            alert: false,
+        });
+    }
+
+    renderPopUp() {
+        return (
+            <Modal
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+                open={this.state.alert}>
+                onClick={this.hideWarning.bind(this)}>
+                        <div><Typography variant="h6" id="modal-title">
+                    asd
+                        </Typography>
+                </div>
+            </Modal>
+        );
+    }
+
     render() {
-        let numericDateString = (date)=>{
+        console.log(this.state.notes);
+        let iconStyle = {
+            fontSize: "16px",
+            align: "right"
+        }
+        let popup= {
+              minHeight: 381,
+              minWidth: 381,
+          };
+        let numericDateString = (date) => {
             let DateObject = new Date(date),
                 numericOptions = {
-                    year: "numeric",
-                    month: "short",
+                    month: "numeric",
                     day: "numeric",
                     hour: "2-digit",
                     minute: "2-digit"
                 };
             return DateObject.toLocaleTimeString("en-US", numericOptions);
         };
-        return(<Grid item md={12}>
+        return (<Grid item md={12}>
             <Grid container spacing={16}>
-                {this.state.notes.map((note,i) => {
-                    return <Grid item md={3} xs={6} key={i}>
+                <Grid item md={3}>
+                    <div className="addNote"
+                        onClick={() => {
+                            this.handleClick();
+                        }}
+                        style={{
+                            "cursor": "pointer",
+                        }}>
+                        <Typography className="center">
+                            <AddIcon />
+                            <br />
+                            Add Note
+                        </Typography>
+                    </div>
+                    <Modal
+                        aria-labelledby="simple-modal-title"
+                        aria-describedby="simple-modal-description"
+                        open={this.state.alert}
+                    >
+                        <div className="exit-popup" style={popup}>
+                            <Grid>
+                            <TextField
+                                id="standard-name"
+                                label="Subject"
+                                className="textfield"
+                                margin="normal"
+                            />
+                            </Grid>
+                            <Grid>
+                            <InputBase
+                                defaultValue={this.state.noteBody}
+                                multiline
+                                inputProps={{ 'aria-label': 'naked' }}
+                            />
+                            </Grid>
+                            <Button>
+                                Delete
+                            </Button>
+                            <Button onClick={this.hideWarning.bind(this)}>
+                                Cancel
+                            </Button>
+                            <Button>
+                                Save
+                            </Button>
+                        </div>
+                    </Modal>
+                </Grid>
+                {this.state.notes.map((note, i) => {
+                    return <Grid item xs={3} key={i}>
                         <Paper className={"note"}>
-                            <div className={"actions"}>
-                                <EditIcon />
-                                <AlertIcon />
-                                <RemoveIcon />
-                            </div>
                             <Typography className={"body"} align={'left'}>
                                 {note.body}
                             </Typography>
-                            <Typography className={"date"} align={'right'}>
-                                {numericDateString(note.timestamp)}
+                            <Typography className={"date"} style={{ fontWeight: "500" }}>
+                                {numericDateString(note.timestamp).replace(",", "・")}
                             </Typography>
+                            <div className={"actions"}>
+                                <EditIcon style={iconStyle}
+                                onClick={() => {
+                                    this.handleClick(note.body);
+                                }}
+                                />
+                                <DoneIcon style={iconStyle} />
+                            </div>
                         </Paper>
                     </Grid>
                 })}
