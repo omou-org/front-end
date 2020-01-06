64d8ce4495a82dc5a3163a3f0bfe5541d6dd0b3c
Daniel Huang <danielhuang@g.ucla.edu>
2020-01-06 13:13:01 -0800

    searching for a parent label for no options available on registration

--- src/components/FeatureViews/Registration/SelectParentDialog.js
+++ src/components/FeatureViews/Registration/SelectParentDialog.js
@@ -152,6 +152,7 @@ class SelectParentDialog extends React.Component {
             <div
                 className={`select-parent-search-wrapper ${this.state.searchingParent ? "active" : ""}`}>
                 <ReactSelect
+                    noOptionsMessage={()=>"Keep searching for a parent!"}
                     classNamePrefix={"select-parent-search"}
                      value = {this.state.inputParent}
                      options = {this.state.parentOptions}
