819b825eb35b3bba161282e3e38edaa40a58b207
Greg Glinoga <46800626+gglinoga@users.noreply.github.com>
2020-03-13 13:23:48 -0700

    remove unused imports, use self closing tag for UnpaidSessions

--- src/components/FeatureViews/AdminPortal/AdminPortalHome.js
+++ src/components/FeatureViews/AdminPortal/AdminPortalHome.js
@@ -4,17 +4,6 @@ import Grid from "@material-ui/core/Grid";
 
 import "./AdminPortal.scss";
 
-import {bindActionCreators} from "redux";
-import * as adminActions from "../../../actions/adminActions";
-import {connect, useDispatch, useSelector} from "react-redux";
-import {Button, Typography} from "@material-ui/core";
-import Paper from "@material-ui/core/Paper";
-import {withRouter} from "react-router-dom";
-import TextField from "@material-ui/core/TextField";
-import Card from "@material-ui/core/Card";
-import {NoListAlert} from "../../NoListAlert";
-import {GET} from "../../../actions/actionTypes";
-import Loading from "../../Loading";
 import UnpaidSessions from "./UnpaidSessions";
 
 function AdminPortalHome() {
