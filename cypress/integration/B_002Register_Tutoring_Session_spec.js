f7896cd18f0894d9ad727655ebf6382127b02458
Daniel Huang <danielhuang@g.ucla.edu>
2020-03-24 19:45:29 -0700

    started register for tutoring session test

--- cypress/integration/B_002Register_Tutoring_Session_spec.js
+++ cypress/integration/B_002Register_Tutoring_Session_spec.js
@@ -2,6 +2,8 @@ describe("B:002Register Tutoring Session", () => {
   it("Set Up", () => {
     cy.login({ email: "maggie@summit.com", password: "password" });
     cy.setRegisteringParent("Janet Mann");
+    cy.contains("REGISTER").click();
+
     cy.exitRegisteringParent();
   });
 });
