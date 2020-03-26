6b66baea4d5c01b5d4bfba52a5183a22788c89ff
Greg Glinoga <46800626+gglinoga@users.noreply.github.com>
2020-03-26 16:08:37 -0700

    rm test file

--- cypress/integration/F_0001Creating_a_new_class_session_spec.js
+++
@@ -1,7 +0,0 @@
-describe("F_0001", () => {
-    it("create a new class sesson", () => {
-        cy.login({email: "gglinoga@gmail.com", password: "password"})
-        cy.contains('Registration').click();
-    })
-})
-
