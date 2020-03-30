bc20b78ad3c6c5e286abb9cfa88c0badc27a1f00
Neil Allavarpu <allavarpuneil@gmail.com>
2020-03-29 20:31:22 -0700

    Update Form.js

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -154,6 +154,7 @@ class Form extends Component {
 				}
 				case "tutoring": {
 					console.log('triggers')
+					console.log("numbah twoo")
 					if (id && this.props.registeredCourses) {
 						if (id.indexOf("+") >= 0) {
 							const studentID = id.substring(0, id.indexOf("+"));
