ef71c2eb986d3655b36cf846a4cf11d4ff4087b4
Wilson Lam <lin-1987@live.com>
2020-05-11 19:01:20 -0700

    removed repeats in the code for 'Add_profile_spec.js'

--- cypress/integration/Add_profile_spec.js
+++ cypress/integration/Add_profile_spec.js
@@ -49,35 +49,4 @@ describe("add_notes_spec", () => {
         cy.contains('AP Computer Science').click();
         cy.contains("Notes").click();
     });
-    it("Will create a note with a subject and body", () => {
-        cy.get("[data-cy=add-note]").click();
-        cy.contains("Save").should("be.disabled");
-        cy.get("[data-cy=textfield]").type("Problem with Stephen Colbert");
-        cy.get("[data-cy=note-body]").type("Stephen is a good kid but he keeps making too many jokes and distracting the class, his work is declining in quality");
-        cy.contains("Save").click();
-        cy.get("[data-cy=note-header]").contains("Problem with Stephen Colbert");
-        cy.get("[data-cy=body]").contains("Stephen is a good kid but he keeps making too many jokes and distracting the class, his work is declining in quality");
-    });
-    it("Will create a note with a notification along with a subject and body", () => {
-        cy.get("[data-cy=add-note]").click();
-        cy.contains("Save").should("be.disabled");
-        cy.get("[data-cy=textfield]").type("Stephen's Improvement");
-        cy.get("[data-cy=note-body]").type("He has taken things more seriously in the past few weeks and his grades have improved immensely!");
-        cy.get("[data-cy=dialog-notification]").click();
-        cy.contains("Save").click();
-        cy.get("[data-cy=note-header]").contains("Stephen's Improvement");
-        cy.get("[data-cy=body]").contains("He has taken things more seriously in the past few weeks and his grades have improved immensely!");
-        cy.get("[data-cy=edit-btn]").last().click();
-        cy.get("[data-cy=dialog-notification]").should('have.css', 'color', 'rgb(255, 0, 0)');
-        cy.contains("Cancel").click();
-    });
-    it("Will check the notes tab if the notes icon has a yellow svg", () => {
-        cy.get("[data-cy=note-number-of-icons]").last().should('exist');
-    })
-    it("Will delete all notes", () => {
-        cy.get("[data-cy=delete-btn]").last().click();
-        cy.get("[data-cy=delete-confirm]").click();
-        cy.get("[data-cy=delete-btn]").first().click();
-        cy.get("[data-cy=delete-confirm]").click();
-    });
 })
