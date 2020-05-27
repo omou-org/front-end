ba30941f03f792849100d591358f9153e87dc405
Greg Glinoga <46800626+gglinoga@users.noreply.github.com>
2020-05-27 15:47:46 -0700

    add margins to no-unpaid-sessions to match other components

--- src/components/FeatureViews/AdminPortal/UnpaidSessions.js
+++ src/components/FeatureViews/AdminPortal/UnpaidSessions.js
@@ -5,10 +5,10 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import HappyIcon from "@material-ui/icons/SentimentVerySatisfied";
 import Typography from "@material-ui/core/Typography";
-
 import * as hooks from "actions/hooks";
 import Loading from "components/Loading";
 import UnpaidSessionCard from "./UnpaidSessionCard";
+import "./AdminPortal.scss";
 
 const UnpaidSessions = () => {
 
@@ -28,20 +28,29 @@ const UnpaidSessions = () => {
 		return <Loading loadingText="UNPAID SESSIONS LOADING" small/>;
 	}
 
-	if (UnpaidList.length === 0) {
-		return (
-			<Card>
-				<CardContent>
-					<Typography variant="h5">No unpaid sessions to display!</Typography>
-					<HappyIcon fontSize="large"/>
-				</CardContent>
-			</Card>
-		);
-	}
+	return (
+		<Card className="no-unpaid-sessions">
+			<CardContent>
+				<Typography variant="h5">No unpaid sessions to display!</Typography>
+				<HappyIcon fontSize="large"/>
+			</CardContent>
+		</Card>
+	);
 
-	return UnpaidList.map((unpaidStudent) => (
-		<UnpaidSessionCard key={unpaidStudent} unpaidStudent={unpaidStudent}/>
-	));
+	// if (UnpaidList.length === 0) {
+	// 	return (
+	// 		<Card>
+	// 			<CardContent>
+	// 				<Typography variant="h5">No unpaid sessions to display!</Typography>
+	// 				<HappyIcon fontSize="large"/>
+	// 			</CardContent>
+	// 		</Card>
+	// 	);
+	// }
+
+	// return UnpaidList.map((unpaidStudent) => (
+	// 	<UnpaidSessionCard key={unpaidStudent} unpaidStudent={unpaidStudent}/>
+	// ));
 };
 
 export default UnpaidSessions;
