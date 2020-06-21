c25ef00ef87f08c25276dd8abe91ec2a0dbae640
jsflca2012 <52120602+jsflca2012@users.noreply.github.com>
2020-06-20 19:41:39 -0700

    newLoading

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import Paper from "@material-ui/core/Paper";
 import Penguin from "./penguin.gif";
 import Typography from "@material-ui/core/Typography";
+import newLoading from "./Comp-1-2.gif";
 
 const Loading = ({
     paper = false, small = false, loadingText = "",
@@ -12,7 +13,7 @@ const Loading = ({
         return (
             <Paper className="paper" style={{"height": "100vh"}}>
                 <Typography variant="h3">{loadingText}</Typography>
-                <img alt="loading penguin" src={Penguin} />
+                <img alt="loading penguin" src={newLoading} style={{fontSize:30, paddingTop:"25%"}}/>
             </Paper>
         );
     }
@@ -30,7 +31,7 @@ const Loading = ({
     }
     return (
         <div className="paper" style={{"height": "70vh"}}>
-            <img alt="loading penguin" src={Penguin} />
+            <img alt="loading penguin" src={newLoading} style={{fontSize:30, paddingTop:"25%"}}/>
             <Typography variant="h3">
                 {loadingText}
             </Typography>
