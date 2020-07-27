721ccee9823c4c9feec4440ff4a9c191a066c3cf
rg <ryan@gedwill.com>
2020-07-27 00:17:42 -0700

    resolved prepop issue

--- src/components/FeatureViews/AdminPortal/ManageCategories2.js
+++ src/components/FeatureViews/AdminPortal/ManageCategories2.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect } from "react";
 import { Admin, ListGuesser, Resource } from "react-admin";
 import {
@@ -17,18 +16,13 @@ import {
 } from "./UserList";
 import { Provider } from "react-redux";
 import { createHashHistory } from "history";
-=======
-import React from "react";
-import {Admin, Resource} from "react-admin";
-import * as components from "./UserList";
-import {Provider} from "react-redux";
-import {createHashHistory} from "history";
->>>>>>> 0cd1ef260d7e6744d4a2fba7ede3868fa79582c4
 import createAdminStore from "../../../createAdminStore";
 import dataProvider from "./dataProvider";
 
+
 const authProvider = () => Promise.resolve();
 const history = createHashHistory();
+
 
 const App = () => (
     <Provider
@@ -38,7 +32,6 @@ const App = () => (
             history,
         })}>
         <Admin dataProvider={dataProvider} history={history}>
-<<<<<<< HEAD
             <Resource edit={CategoryEdit} create={CategoryCreate} list={CategoryList}
                 name="courseCategories" show={CategoryShow} />
             <Resource edit={SchoolEdit} create={SchoolCreate} list={SchoolList}
@@ -46,28 +39,6 @@ const App = () => (
             <Resource edit={TuitionEdit} create={TuitionCreate} list={TuitionList}
                 name="tuitionRules" show={TuitionShow} />
 
-=======
-            <Resource create={components.CategoryCreate}
-                edit={components.CategoryEdit} list={components.CategoryList}
-                name="courseCategories" show={components.CategoryShow} />
-            <Resource create={components.SchoolCreate}
-                edit={components.SchoolEdit} list={components.SchoolList}
-                name="schools" show={components.SchoolShow} />
-            <Resource create={components.BulkDiscountCreate}
-                edit={components.BulkDiscountEdit}
-                list={components.BulkDiscountList}
-                name="bulkDiscounts" show={components.BulkDiscountShow} />
-            <Resource create={components.DateRangeDiscountCreate}
-                edit={components.DateRangeDiscountEdit}
-                list={components.DateRangeDiscountList}
-                name="dateRangeDiscounts"
-                show={components.DateRangeDiscountShow} />
-            <Resource create={components.PaymentMethodDiscountCreate}
-                edit={components.PaymentMethodDiscountEdit}
-                list={components.PaymentMethodDiscountList}
-                name="paymentMethodDiscounts"
-                show={components.PaymentMethodDiscountShow} />
->>>>>>> 0cd1ef260d7e6744d4a2fba7ede3868fa79582c4
         </Admin>
     </Provider>
 );
--- src/components/FeatureViews/AdminPortal/UserList.js
+++ src/components/FeatureViews/AdminPortal/UserList.js
@@ -1,146 +1,170 @@
+/* eslint-disable indent */
 import * as React from "react";
 import {
-    Datagrid, Edit, List, Show, SimpleForm, SimpleShowLayout, TextField,
-    TextInput, Create, SelectField, SelectInput, ReferenceInput
+  Datagrid,
+  Edit,
+  List,
+  Show,
+  SimpleForm,
+  SimpleShowLayout,
+  TextField,
+  TextInput,
+  Create,
+  SelectField,
+  SelectInput,
+  ReferenceInput,
 } from "react-admin";
 
 export const CategoryList = (props) => (
-    <List {...props}>
-        <Datagrid rowClick="edit">
-            <TextField source="id" />
-            <TextField source="name" />
-            <TextField source="description" />
-        </Datagrid>
-    </List>
+  <List {...props}>
+    <Datagrid rowClick="edit">
+      <TextField source="id" />
+      <TextField source="name" />
+      <TextField source="description" />
+    </Datagrid>
+  </List>
 );
 
 export const CategoryCreate = (props) => (
-    <Create {...props}>
-        <SimpleForm>
-            <TextInput source="name" />
-            <TextInput source="description" />
-        </SimpleForm>
-    </Create>
+  <Create {...props}>
+    <SimpleForm>
+      <TextInput source="name" />
+      <TextInput source="description"></TextInput>
+    </SimpleForm>
+  </Create>
 );
 
 export const CategoryEdit = (props) => (
-    <Edit {...props}>
-        <SimpleForm>
-            <TextInput source="name" />
-            <TextInput source="description" />
-        </SimpleForm>
-    </Edit>
+  <Edit {...props}>
+    <SimpleForm>
+      <TextInput source="name" />
+      <TextInput source="description" />
+    </SimpleForm>
+  </Edit>
 );
 
 export const CategoryShow = (props) => (
-    <Show {...props}>
-        <SimpleShowLayout>
-            <TextField source="id" />
-            <TextField source="description" />
-            <TextField source="name" />
-        </SimpleShowLayout>
-    </Show>
+  <Show {...props}>
+    <SimpleShowLayout>
+      <TextField source="id" />
+      <TextField source="description" />
+      <TextField source="name" />
+    </SimpleShowLayout>
+  </Show>
 );
 
-// Schools
+//Schools
 export const SchoolList = (props) => (
-    <List {...props}>
-        <Datagrid rowClick="edit">
-            <TextField source="id" />
-            <TextField source="name" />
-            <TextField source="zipcode" />
-            <TextField source="district" />
-        </Datagrid>
-    </List>
+  <List {...props}>
+    <Datagrid rowClick="edit">
+      <TextField source="id" />
+      <TextField source="name" />
+      <TextField source="zipcode" />
+      <TextField source="district" />
+    </Datagrid>
+  </List>
 );
 
 export const SchoolCreate = (props) => (
-    <Create {...props}>
-        <SimpleForm>
-            <TextInput source="name" />
-            <TextInput source="zipcode" />
-            <TextInput source="district" />
-        </SimpleForm>
-    </Create>
+  <Create {...props}>
+    <SimpleForm>
+      <TextInput source="name" />
+      <TextInput source="zipcode" />
+      <TextInput source="district" />
+    </SimpleForm>
+  </Create>
 );
 
 export const SchoolShow = (props) => (
-    <Show {...props}>
-        <SimpleShowLayout>
-            <TextField source="id" />
-            <TextField source="name" />
-            <TextField source="zipcode" />
-            <TextField source="district" />
-        </SimpleShowLayout>
-    </Show>
+  <Show {...props}>
+    <SimpleShowLayout>
+      <TextField source="id" />
+      <TextField source="name" />
+      <TextField source="zipcode" />
+      <TextField source="district" />
+    </SimpleShowLayout>
+  </Show>
 );
 
 export const SchoolEdit = (props) => (
-    <Edit {...props}>
-        <SimpleForm>
-            <TextInput source="name" />
-            <TextInput source="zipcode" />
-            <TextInput source="district" />
-        </SimpleForm>
-    </Edit>
+  <Edit {...props}>
+    <SimpleForm>
+      <TextField source="name" />
+      <TextField source="zipcode" />
+      <TextField source="district" />
+    </SimpleForm>
+  </Edit>
 );
 
 const academicLevelChoices = [
-    { id: 'ELEMENTARY_LVL', name: 'Elementary School'},
-    { id: 'middle_lvl', name: 'Middle School'},
-    { id: 'HIGH_LVL', name: 'High School' },
-    { id: 'college_lvl', name: 'College' },
+  { id: "ELEMENTARY_LVL", name: "Elementary School" },
+  { id: "MIDDLE_LVL", name: "Middle School" },
+  { id: "HIGH_LVL", name: "High School" },
+  { id: "COLLEGE_LVL", name: "College" },
 ];
 const courseTypes = [
-    {id: 'CLASS', name: 'Class'},
-    {id: 'TUTORING', name:'Tutoring'},
-    {id: 'SMALL_GROUP', name:'Small Group'}
-]
+  { id: "CLASS", name: "Class" },
+  { id: "TUTORING", name: "Tutoring" },
+  { id: "SMALL_GROUP", name: "Small Group" },
+];
 
 export const TuitionList = (props) => (
-    <List {...props}>
-        <Datagrid rowClick="edit">
-            <TextField source="id"/>
-            <SelectField source="academicLevel" choices={academicLevelChoices} />
-            <TextField source="name"/>
-            <SelectField source="courseType" choices={courseTypes}/>
-            <TextField source="category.name" />
-
-        </Datagrid>
-    </List>
+  <List {...props}>
+    <Datagrid rowClick="edit">
+      <TextField source="id" />
+      <SelectField source="academicLevel" choices={academicLevelChoices} />
+      <TextField source="name" />
+      <SelectField source="courseType" choices={courseTypes} />
+      <TextField source="category.name" label="Category"/>
+    </Datagrid>
+  </List>
 );
 
 export const TuitionShow = (props) => (
-    <Show {...props}>
-        <SimpleShowLayout>
-            <TextField source="id"/>
-            <TextField source="academicLevel" />
-            <TextField source="name" />
-        </SimpleShowLayout>
-    </Show>
+  <Show {...props}>
+    <SimpleShowLayout>
+      <TextField source="id" />
+      <TextField source="academicLevel" />
+      <TextField source="name" />
+    </SimpleShowLayout>
+  </Show>
 );
-
-
 
 export const TuitionEdit = (props) => (
-    <Edit {...props}>
-        <SimpleForm>
-            <TextField source="name" />
-            <SelectField source="academicLevel" choices={academicLevelChoices} />
-        </SimpleForm>
-    </Edit>
+  <Edit {...props}>
+    <SimpleForm>
+      <TextInput source="name" />
+      <SelectInput source="academicLevel" choices={academicLevelChoices} />
+      <ReferenceInput
+        label="Course Category"
+        // source="category"
+        source="category.id"
+        reference="courseCategories"
+        >
+            <SelectInput 
+              optionText={(record) => record.name} 
+              />
+        </ReferenceInput>
+        <TextInput source="hourlyTuition" />
+        <SelectInput source="courseType" choices={courseTypes} />
+    </SimpleForm>
+  </Edit>
 );
 
-
 export const TuitionCreate = (props) => (
-    <Create {...props}>
-        <SimpleForm>
-            <TextInput source="name" />
-            <SelectInput source="academicLevel" choices={academicLevelChoices} />
-            <ReferenceInput label="courseCategory" source="id" reference="courseCategories" >
-                <SelectInput optionText="courseCategory" />
-            </ReferenceInput>
-            <SelectInput source="courseType" choices={courseTypes} />
-        </SimpleForm>
-    </Create>
+  <Create {...props}>
+    <SimpleForm>
+      <TextInput source="name" />
+      <SelectInput source="academicLevel" choices={academicLevelChoices} />
+      <ReferenceInput
+        label="Course Category"
+        source="category"
+        reference="courseCategories"
+      >
+        <SelectInput optionText={(record) => record.name} />
+      </ReferenceInput>
+      <TextInput source="hourlyTuition" />
+      <SelectInput source="courseType" choices={courseTypes} />
+    </SimpleForm>
+  </Create>
 );
