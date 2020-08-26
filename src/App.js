1f4c55f8ed40d2a026a0a7f313803da3ae311d0b
rg <ryan@gedwill.com>
2020-08-25 19:15:13 -0700

    session logout after 20 minutes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 /* eslint-disable indent */
 
-import React, { useEffect, useState, useMemo} from "react";
+import React, { useEffect, useState, useMemo, useCallback} from "react";
+import { useDispatch }  from "react-redux";
+import { logout } from "actions/authActions";
+import { closeRegistrationCart } from "components/OmouComponents/RegistrationUtils";
+
+
 import { format } from 'date-fns';
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Moment from "react-moment";
@@ -8,26 +13,82 @@ import momentTimezone from "moment-timezone";
 import moment from "moment";
 import Navigation from "./components/Navigation/Navigation";
 import { Modal } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 import "./theme/theme.scss";
 import MuiPickersUtilsProvider from "@material-ui/pickers/MuiPickersUtilsProvider";
 import MomentUtils from "@date-io/moment";
 import {useIdleTimer} from 'react-idle-timer';
+import { useHistory } from "react-router-dom";
 
 Moment.globalMoment = momentTimezone;
 Moment.globalTimezone = "America/Los_Angeles";
 
+function rand() {
+	return Math.round(Math.random() * 20) - 10;
+  }
 
+
+function getModalStyle() {
+	const top = 50 + rand();
+	const left = 50 + rand();
+  
+	return {
+	  top: `${top}%`,
+	  left: `${left}%`,
+	  transform: `translate(-${top}%, -${left}%)`,
+	};
+  }
+
+const useStyles = makeStyles((theme) => ({
+	paper: {
+			position: 'absolute',
+			width: 400,
+			backgroundColor: theme.palette.background.paper,
+			border: '2px solid #000',
+			boxShadow: theme.shadows[5],
+			padding: theme.spacing(2, 4, 3),
+	},
+}));
 
 const App = () => {
-		const timeout = 3000;
+		const history = useHistory();
+		const dispatch = useDispatch();
+
+		const handleLogout = useCallback(() => {
+			console.log("logging out...")
+			closeRegistrationCart();
+			dispatch(logout());
+			history.push("/login");
+		}, [dispatch, history]);
+
+		let timeout = 10000; //1080000;
+		const modalTimeout = 5000 //120000;
 		const [remaining, setRemaining] = useState(timeout);
 		const [elapsed, setElapsed] = useState(0);
+		const elapseDiff = 5000;
+
 		const [lastActive, setLastActive] = useState(+new Date());
 		const [isIdle, setIsIdle] = useState(false);
 
 		const handleOnActive = () => setIsIdle(false);
 		const handleOnIdle = () => setIsIdle(true);
+
+		const classes = useStyles();
+		// getModalStyle is not a pure function, we roll the style only on the first render
+		const [modalStyle] = useState(getModalStyle);
+		const [open, setOpen] = useState(false);
+
+		const handleOpen = () => {
+			setOpen(true);
+			handleReset();
+			setRemaining(modalTimeout);
+		  };
+		
+		  const handleClose = () => {
+			console.log("closing");
+			setOpen(false);
+		  };
 
 		const {
 				reset,
@@ -58,47 +119,41 @@ const App = () => {
 				}, 1000)
 		}, []);
 
-		const idle20Minutes = () => {
-				return elapsed > 10000;
-		};
-		const useStyles = makeStyles((theme) => ({
-				paper: {
-						position: 'absolute',
-						width: 400,
-						backgroundColor: theme.palette.background.paper,
-						border: '2px solid #000',
-						boxShadow: theme.shadows[5],
-						padding: theme.spacing(2, 4, 3),
-				},
-		}));
-		const handleOpen = () => {
-			setOpen(true);
-		  };
-		
-		  const handleClose = () => {
-			setOpen(false);
-		  };
-
-		const modalBody = (
-				<div style={modalStyle}>
-
-				</div>
-		);
+		const body = () => {
+			return (
+			<div style={modalStyle} className={classes.paper}>
+			  <p id="simple-modal-description">
+				{
+					(remaining === 100) && handleClose()
+					// remaining <= 5000 ? handleLogout() : doNothing()
+				}
+				Are you still there?
+				<button onClick={handleClose}>Yes</button>
+			  </p>
+			</div>
+		  )};
 
 		return (
 				<div>
 					<div>
-						<button onClick={handleOpen}>Open Modal</button>
-						<Modal
-							open={open}
-							onClose={handleClose}
-							aria-labelledby="simple-modal-title"
-							aria-describedby="simple-modal-description"
-						></Modal>
+					<button type="button" onClick={handleOpen}>
+						Open Modal
+					</button>
+					{
+						(remaining === 0) && handleOpen()
+						// remaining === 0 ? handleOpen() : doNothing()
+					}
+				<Modal
+					open={open}
+					onClose={handleClose}
+					aria-labelledby="simple-modal-title"
+					aria-describedby="simple-modal-description"
+				>
+					{body()}
+				</Modal>
 
 
 						<button onClick={handleReset}>Yes</button>
-
 						<div>
 							<button onClick={handleReset}>RESET</button>
 							<button onClick={handlePause}>PAUSE</button>
