3b631dfb13d046c859ec10e52b2842bf1c8e5a56
Wilson Lam <lin-1987@live.com>
2020-09-18 13:37:18 -0700

    instructor test and edit complete

--- cypress/integration/User Management/BJ_00_InstructorEdit.js
+++ cypress/integration/User Management/BJ_00_InstructorEdit.js
@@ -1,5 +1,3 @@
-const { it } = require("date-fns/locale");
-
 describe("Fills out form", () => {
     before(() => {
         cy.fixture("users.json").then(({instructor}) => {
@@ -48,32 +46,67 @@ describe("Fills out form", () => {
     });
 
     it("Can enter data properly into the first form", () => {
-        cy.fixture("users.json").then(({instructor}) => {
+        cy.fixture("users.json").then(({instructor2}) => {
             cy.get("[data-cy=basicInfo-phoneNumber-input]")
                 .clear()
                 .fastType("0");
             cy.get("[data-cy=basicInfo-nextButton]").should("be.disabled");
             cy.get("[data-cy=basicInfo-phoneNumber-input]").clear();
-            cy.get("[data-cy=basicInfo-phoneNumber-input]").fastType(instructor.phoneNumber);
+            cy.get("[data-cy=basicInfo-phoneNumber-input]").fastType(instructor2.phoneNumber);
+            cy.get("[data-cy=basicInfo-address-input]")
+                .clear()
+                .fastType(instructor2.address)
+                .should("have.value", instructor2.address)
+            cy.get("[data-cy=basicInfo-nextButton]").should("be.enabled");
+        }); 
+    });
+
+    it("Properly goes to the next part of the form and checks for data to load properly on the second form", () => {
+        cy.fixture("users.json").then(({instructor}) => {
+            cy.get("[data-cy=basicInfo-nextButton]").click();
+            cy.get("[data-cy=experience-subjects]")
+                .children()
+                .find("span")
+                .first()
+                .should("have.text", instructor.subjects[0].name);
+            cy.get("[data-cy=experience-experience-input")
+                .should("have.value", instructor.experience);
+            cy.get("[data-cy=experience-biography-input]")
+                .should("have.value", instructor.biography);
+            cy.get("[data-cy=experience-language-input]")
+                .should("have.value", instructor.language);
             cy.get("[data-cy=basicInfo-nextButton]").should("be.enabled");
         });
     });
 
-    // it("Properly goes to the next part of the form and checks for data to load properly on the second form", () => {
-    //     cy.fixture("users.json").then(({instructor}) => {
-    //         cy.get("[data-cy=basicInfo-nextButton]").click();
-    //         cy.get("[data-cy=experience-subjects]")
-    //             .children()
-    //             .find("span")
-    //             .first()
-    //             .should("have.text", instructor.subjects[0].name);
-    //         cy.get("[data-cy=experience-experience-input")
-    //             .should("have.value", "5");
-    //         cy.get("[data-cy=experience-biography-input]")
-    //             .should("have.value", instructor.biography);
-    //         cy.get("[data-cy=experience-language-input]")
-    //             .should("have.value", instructor.language);
-    //         cy.get("[data-cy=basicInfo-nextButton]").should("be.enabled");
-    //     });
-    // });
+    it("Can enter data properly into the second form and go back to and from the first form with all data intact", () => {
+        cy.fixture("users.json").then(({instructor2}) => {
+            cy.get("[data-cy=backButton]")
+                .should("be.enabled")
+                .click()
+            cy.get("[data-cy=basicInfo-nextButton]")
+                .should("be.enabled")
+                .click();            
+            cy.get("[data-cy=experience-experience-input")
+                .clear()
+                .should("be.empty")
+                .fastType(instructor2.experience)
+                .should("have.value", instructor2.experience)
+            cy.get("[data-cy=experience-biography-input")
+                .clear()
+                .should("be.empty")
+                .fastType(instructor2.biography)
+                .should("have.value", instructor2.biography)
+            cy.get("[data-cy=experience-language-input]")
+                .clear()
+                .should("be.empty")
+                .fastType(instructor2.language)
+                .should("have.value", instructor2.language)
+                .parent()
+                .siblings("label")
+                .should("have.css", "color", "rgb(67, 181, 217)")
+            cy.get("[data-cy=submitButton]")
+                .should("be.enabled")
+        });
+    });
 });
