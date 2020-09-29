badca6bc268e931320aafa5afb90e33e6cff956b
Peter Kang <peeterkang@gmail.com>
2020-09-29 14:26:01 -0700

    used visitAuthenticated instead of login for test

--- cypress/integration/Add_Item_Button.js
+++ cypress/integration/Add_Item_Button.js
@@ -1,6 +1,5 @@
 describe("Sets a height of the AddItemButton component", () => {
 	it("sets a height of the AddItemButtonComponent", () => {
-		cy.login({email: "maggie@summit.com", password: "password"});
-		cy.contains("Accounts").click();
+		cy.visitAuthenticated(`/accounts/parent/2`)
 	});
 });
