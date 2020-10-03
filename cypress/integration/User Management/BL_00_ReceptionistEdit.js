40ace2c65623a56b287b73b857c9ad318a7aff9a
Wilson Lam <lin-1987@live.com>
2020-10-03 11:09:15 -0700

    created updated receptionist data and renamed tests to reflect PR's comments

--- cypress/integration/User Management/BL_00_ReceptionistEdit.js
+++ cypress/integration/User Management/BL_00_ReceptionistEdit.js
@@ -1,6 +1,6 @@
-describe("Fills out form with mock data of receptionist from our user.json file, view our form to see if the data is inputed properly, edit our form with edited data, and submits our form", () => {
+describe("Editing an existing Receptionist account details", () => {
     before(() => {
-        cy.fixture("users.json").then(({ receptionist }) => {
+        cy.fixture("users.json").then(({ original_receptionist_data, updated_receptionist_data }) => {
             cy.mockGraphQL({
                 "CreateAdmin": {
                     "response": {
@@ -13,7 +13,7 @@ describe("Fills out form with mock data of receptionist from our user.json file,
                     "test": (variables) => {
                         Object.entries(variables).forEach(([key, value]) => {
                             if (!["phoneNumber", "id", "user", "password"].includes(key)) {
-                                expect(receptionist[key] || receptionist?.user[key]).equals(value);
+                                expect(updated_receptionist_data[key] || updated_receptionist_data?.user[key]).equals(value);
                             }
                         });
                     },
@@ -22,107 +22,102 @@ describe("Fills out form with mock data of receptionist from our user.json file,
                     "response": {
                         "data": {
                             "admin": {
-                                ...receptionist,
+                                ...original_receptionist_data,
                                 "password": null,
                             },
                         },
                     },
                     "test": ({ userID }) => {
-                        expect(userID).equals(receptionist.user.id.toString(), "Check ID passed");
+                        expect(userID).equals(original_receptionist_data.user.id.toString(), "Check ID passed");
                     },
                 },
             });
-            cy.visitAuthenticated(`/form/admin/${receptionist.user.id}`);
+            cy.visitAuthenticated(`/form/admin/${original_receptionist_data.user.id}`);
         });
     });
 
-    it("Loads our mock data properly into the login details form section for receptionist", () => {
-        cy.fixture("users.json").then(({ receptionist }) => {
+    it("Loads existing receptionist account login details form section for receptionist", () => {
+        cy.fixture("users.json").then(({ original_receptionist_data }) => {
             cy.get("[data-cy=login-firstName-input]")
-                .should("have.value", receptionist.user.firstName);
+                .should("have.value", original_receptionist_data.user.firstName);
             cy.get("[data-cy=login-lastName-input]")
-                .should("have.value", receptionist.user.lastName);
+                .should("have.value", original_receptionist_data.user.lastName);
             cy.get("[data-cy=login-email-input]")
-                .should("have.value", receptionist.user.email);
+                .should("have.value", original_receptionist_data.user.email);
             cy.get("[data-cy=login-nextButton]")
                 .should("be.disabled");
         });
     });
 
-    it("Can enter data from the user properly into the first form, while checking if bad info is entered the submit button should be disabled", () => {
-        cy.fixture("users.json").then(({ receptionist }) => {
-            cy.get("[data-cy=login-firstName-input]")
+    it("Edit receptionist data in Login Details section of the form", () => {
+        cy.fixture("users.json").then(({ updated_receptionist_data }) => {
+            cy.get("[data-cy=login-email-input]")
                 .clear()
-            cy.get("[data-cy=login-firstName-input]")
-                .fastType("0");
             cy.get("[data-cy=login-nextButton]")
                 .should("be.disabled");
-            cy.get("[data-cy=login-firstName-input]")
-                .clear();
-            cy.get("[data-cy=login-firstName-input]")
-                .fastType(receptionist.user.firstName);
+            cy.get("[data-cy=login-email-input]")
+                .fastType(updated_receptionist_data.user.email);
             cy.get("[data-cy=login-nextButton]")
                 .should("be.disabled");
             cy.get("[data-cy=login-password-input]")
                 .should("be.empty")
-                .fastType(0)
-                .clear()
                 .fastType("test1234")
                 .should("have.value", "test1234");
         });
     });
 
-    it("Properly goes to the next section of the form called User Information and checks for data in stubs to load properly on the second form", () => {
-        cy.fixture("users.json").then(({ receptionist }) => {
+
+
+    it("Loads receptionist data into User Information section of receptionist editing form", () => {
+        cy.fixture("users.json").then(({ original_receptionist_data }) => {
             cy.get("[data-cy=login-nextButton")
                 .should("be.enabled")
                 .click();
             cy.get("[data-cy=user-adminType-select]")
                 .siblings()
-                .should("have.value", receptionist.adminType);
+                .should("have.value", original_receptionist_data.adminType);
             cy.get("[data-cy=user-address-input]")
-                .should("have.value", receptionist.address);
+                .should("have.value", original_receptionist_data.address);
             cy.get("[data-cy=user-phoneNumber-input]")
-                .should("have.value", receptionist.phoneNumber);
+                .should("have.value", original_receptionist_data.phoneNumber);
             cy.get("[data-cy=submitButton]")
                 .should("be.enabled");
         });
     });
 
-    it("Should verify if the data from the user on form 1 still persists if we accidently click the back button", () => {
+    it("Verifies data from the user on form 1 persists if back button is clicked", () => {
+        cy.fixture("users.json").then(({ updated_receptionist_data }) => {
             cy.get("[data-cy=backButton]")
                 .should("be.enabled")
                 .click();
-            cy.get("[data-cy=login-password-input]")
-                .should("have.value", "test1234");
+            cy.get("[data-cy=login-email-input]")
+                .should("have.value", updated_receptionist_data.user.email);
+            cy.get("[data-cy=login-nextButton]")
+                .should("be.enabled")
+                .click();  
+        })
     });
 
 
-    it("Can enter data properly into the User Information form and go back to and from the first form with all data intact", () => {
-        cy.fixture("users.json").then(({ receptionist }) => {
-            cy.get("[data-cy=login-nextButton]")
-                .should("be.enabled")
-                .click();   
-            cy.get("[data-cy=user-phoneNumber-input]")
+    it("Edit receptionist data in Login Edit section of the form", () => {
+        cy.fixture("users.json").then(({ updated_receptionist_data }) => { 
+            cy.get("[data-cy=user-address-input")
                 .clear()
-                .should("be.empty")
-                .fastType(0);
-            cy.get("[data-cy=submitButton]")
-                .should("be.disabled");
-            cy.get("[data-cy=user-phoneNumber-input")
+                .fastType(updated_receptionist_data.address)
+                .should("have.value", updated_receptionist_data.address);
+            cy.get("[data-cy=user-zipcode-input")
                 .clear()
-                .fastType(receptionist.phoneNumber)
-                .should("have.value", receptionist.phoneNumber);
+                .fastType(updated_receptionist_data.zipcode);
             cy.get("[data-cy=submitButton]")
                 .should("be.enabled");
         });
     });
 
-    it("Properly submits the receptionist form and displays the results page", () => {
-        cy.fixture("users.json").then(({ receptionist }) => {
+    it("Submits the receptionist form and displays the results page", () => {
+        cy.fixture("users.json").then(({ updated_receptionist_data }) => {
             cy.get("[data-cy=submitButton]").click();
             cy.contains("submitted");
-            cy.contains(receptionist.phoneNumber);
+            cy.contains(updated_receptionist_data.address);
         });
     });
 });
