618204133b97d1ca28121c5a4e9e376d662d79d4
Andrew Knapp <ajknapp2015@gmail.com>
2020-10-04 13:44:04 -0700

    Added EnrollmentPayment.js to handle fetching payment data for the PaymentTable in EnrollmentView.js

---
+++ src/components/FeatureViews/Accounts/TabComponents/EnrollmentPayment.js
@@ -0,0 +1,50 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Loading from "components/OmouComponents/Loading";
+import PaymentTable from "./PaymentTable";
+import {useQuery} from "@apollo/react-hooks";
+import Typography from "@material-ui/core/Typography";
+import gql from "graphql-tag";
+
+export const GET_PARENT_PAYMENTS = gql`
+    query ParentPayments($parentId: ID!) {
+        payments(parentId: $parentId) {
+            id
+            createdAt
+            registrationSet {
+              id
+            }
+            total
+            method
+        }
+    }
+`;
+
+const EnrollmentPayment = ({user_id}) => {
+	const {data, loading, error} = useQuery(GET_PARENT_PAYMENTS,
+		{variables: {parentId: user_id}}
+	);
+
+	if (loading) {
+		return <Loading/>
+	}
+	if (error) {
+		return <Typography>
+			There's been an error! Error: {error.message}
+		</Typography>
+    }
+
+	const {payments} = data;
+
+    return (
+        <PaymentTable
+            paymentList={payments}
+            type="parent" />
+    );
+};
+
+EnrollmentPayment.propTypes = {
+    "user_id": PropTypes.number.isRequired,
+};
+
+export default EnrollmentPayment;
