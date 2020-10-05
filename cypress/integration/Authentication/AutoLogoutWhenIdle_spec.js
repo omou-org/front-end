963b8eca3f97b136ff99f3ee9df7d0f3e38276de
rg <ryan@gedwill.com>
2020-10-04 19:31:48 -0700

    idle logout tests and admin stubbed

--- cypress/integration/Authentication/AutoLogoutWhenIdle_spec.js
+++ cypress/integration/Authentication/AutoLogoutWhenIdle_spec.js
@@ -1,11 +1,13 @@
+const { it } = require("date-fns/locale");
+
 describe("Fills out form", () => {
     before(() => {
-        cy.fixture("users.json").then(({parent}) => {
+        cy.fixture("users.json").then(({admin}) => {
             cy.mockGraphQL({
-                "CreateParentAccount": {
+                "CreateAdmin": {
                     "response": {
                         "data": {
-                            "createParent": {
+                            "createAdmin": {
                                 "created": false,
                             },
                         },
@@ -13,27 +15,40 @@ describe("Fills out form", () => {
                     "test": (variables) => {
                         Object.entries(variables).forEach(([key, value]) => {
                             if (!["phoneNumber", "id", "user", "password"].includes(key)) {
-                                expect(parent[key] || parent?.user[key]).equals(value);
+                                expect(admin[key] || admin?.user[key]).equals(value);
                             }
                         });
                     },
                 },
-                "GetParent": {
+                "GetAdmin": {
                     "response": {
                         "data": {
-                            "parent": {
-                                ...parent,
+                            // "parent": {
+                                ...admin,
                                 "phoneNumber": null,
                             },
                         },
                     },
                     "test": ({id}) => {
-                        expect(id).equals(parent.user.id.toString(), "Check ID passed");
+                        expect(id).equals(admin.user.id.toString(), "Check ID passed");
                     },
                 },
             });
-            cy.visitAuthenticated(`/form/parent/${parent.user.id}`);
+            cy.visitAuthenticated(`/form/admin/${admin.user.id}`);
         });
+    
     });
+
+    it("idles for 18 mins and clicks still here", () => {
+        setTimeout(function() {console.log("waiting 18 mins...");}, 18000);
+        cy.get("[data-cy=activityCheckModal]").should("be.enabled");
+        cy.get("[data-cy=activityModalSubmit]").click()
+        cy.get("[data-cy=hiddenTimer]").should("have.value", 0);
+    };
+    it("idles for 20 mins and logs out", () => {
+        setTimeout(function() { console.log("waiting for 20 mins..."); }, 20000);
+        cy.get("[data-cy=login-firstName-input]")
+            .should("be.empty");
+    })
 
 });
