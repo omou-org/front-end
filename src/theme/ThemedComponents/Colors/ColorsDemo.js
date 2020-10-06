be9319ebace89f25e7c4476537e30892c5334870
juicekey <mr.chrislojewski@gmail.com>
2020-10-06 13:03:56 -0700

    Made demo page

---
+++ src/theme/ThemedComponents/Colors/ColorsDemo.js
@@ -0,0 +1,170 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid';
+import { Typography, Box } from '@material-ui/core';
+import BackgroundPaper from '../../../components/OmouComponents/BackgroundPaper';
+import theme from '../../muiTheme'
+
+const ColorBox = ({bgcolor}) => {
+    return (
+        <Box
+            bgcolor={bgcolor}
+            width="98px"
+            height="48px"
+            label={bgcolor}
+            boxShadow={2}
+            borderRadius={5}
+            mt={2}
+            mb={1}
+            ml={2}
+            mr={2}
+        />
+    )
+}
+
+const ColorsDemo = () => {
+    return (
+        <BackgroundPaper>
+
+        <Grid container justify="space-around">
+            <Grid item>
+            <Grid container>
+                <Typography variant="h3">PRIMARY</Typography>
+                <Grid item>
+                <Grid container direction="column">
+                    <Grid item>
+                        <ColorBox
+                            bgcolor={theme.colors.darkBlue}
+                        />
+                        <Typography>Dark Blue</Typography>
+                        <Typography>{theme.colors.darkBlue}</Typography>
+                    </Grid>
+                    <Grid item>
+                        <ColorBox
+                            bgcolor={theme.colors.omouBlue}
+                        />
+                        <Typography>Omou Blue</Typography>
+                        <Typography>{theme.colors.omouBlue}</Typography>
+                    </Grid>
+                    <Grid item>
+                        <ColorBox
+                            bgcolor={theme.colors.skyBlue}
+                        />
+                        <Typography>Sky Blue</Typography>
+                        <Typography>{theme.colors.skyBlue}</Typography>
+                    </Grid>
+                    <Grid item>
+                        <ColorBox
+                            bgcolor={theme.colors.white}
+                        />
+                        <Typography>White</Typography>
+                        <Typography>{theme.colors.white}</Typography>
+                    </Grid>
+                </Grid>
+                </Grid>
+
+                <Grid item>
+                <Grid container direction="column">
+                    <Grid item>
+                        <ColorBox
+                            bgcolor={theme.colors.goth}
+                        />
+                        <Typography>Goth</Typography>
+                        <Typography>{theme.colors.goth}</Typography>
+                    </Grid>
+                    <Grid item>
+                        <ColorBox
+                            bgcolor={theme.colors.charcoal}
+                        />
+                        <Typography>Charcoal</Typography>
+                        <Typography>{theme.colors.charcoal}</Typography>
+                    </Grid>
+                    <Grid item>
+                        <ColorBox
+                            bgcolor={theme.colors.slateGrey}
+                        />
+                        <Typography>Slate Grey</Typography>
+                        <Typography>{theme.colors.slateGrey}</Typography>
+                    </Grid>
+                    <Grid item>
+                        <ColorBox
+                            bgcolor={theme.colors.gloom}
+                        />
+                        <Typography>Gloom</Typography>
+                        <Typography>{theme.colors.gloom}</Typography>
+                    </Grid>
+                    <Grid item>
+                        <ColorBox
+                            bgcolor={theme.colors.cloudy}
+                        />
+                        <Typography>Cloudy</Typography>
+                        <Typography>{theme.colors.cloudy}</Typography>
+                    </Grid>
+                    <Grid item>
+                        <ColorBox
+                            bgcolor={theme.colors.lightGrey}
+                        />
+                        <Typography>Light Grey</Typography>
+                        <Typography>{theme.colors.lightGrey}</Typography>
+                    </Grid>
+                </Grid>
+                </Grid>
+            </Grid>
+            </Grid>
+
+            <Grid item>
+            <Grid container>
+                <Typography variant="h3">SPECIFIC</Typography>
+                <Grid item>
+                <Grid container direction="column">
+                    <Grid item>
+                        <ColorBox
+                            bgcolor={theme.colors.buttonBlue}
+                        />
+                        <Typography>Button Blue</Typography>
+                        <Typography>{theme.colors.buttonBlue}</Typography>
+                    </Grid>
+                    <Grid item>
+                        <ColorBox
+                            bgcolor={theme.colors.backgroundGrey}
+                        />
+                        <Typography>Background Grey</Typography>
+                        <Typography>{theme.colors.backgroundGrey}</Typography>
+                    </Grid>
+                </Grid>
+                </Grid>
+
+                <Grid item>
+                    <Grid container direction="column">
+                        <Grid item>
+                            <ColorBox
+                                bgcolor="#6CE086"
+                            />
+                            <Typography>Success</Typography>
+                            <Typography>#6CE086</Typography>
+                        </Grid>
+                        <Grid item>
+                            <ColorBox
+                                bgcolor="#FFDD59"
+                            />
+                            <Typography>Warning</Typography>
+                            <Typography>#FFDD59</Typography>
+                        </Grid>
+                        <Grid item>
+                            <ColorBox
+                                bgcolor="#FF6766"
+                            />
+                            <Typography>Error</Typography>
+                            <Typography>#FF6766</Typography>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+            </Grid>
+        </Grid>
+        
+        </BackgroundPaper>
+
+    )
+}
+
+export default ColorsDemo;
