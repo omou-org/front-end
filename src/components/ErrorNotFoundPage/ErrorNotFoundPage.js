2c2fd5d5cdfda2514a5f0083c6be706765113206
Andrew Knapp <ajknapp2015@gmail.com>
2020-11-16 23:16:41 -0800

    Removed paper from 404 page and modified associated scss file

--- src/components/ErrorNotFoundPage/ErrorNotFoundPage.js
+++ src/components/ErrorNotFoundPage/ErrorNotFoundPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {useHistory} from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 import Button from "@material-ui/core/Button";
 import Paper from "@material-ui/core/Paper";
@@ -8,20 +8,18 @@ import Typography from "@material-ui/core/Typography";
 import "./ErrorNotFoundPage.scss";
 
 const ErrorNotFoundPage = () => {
-	const {goBack} = useHistory();
+	const { goBack } = useHistory();
 	return (
 		<div className="error-page">
-			<Paper elevation={2} className="paper">
-				<Typography className="center">
-					404.
-					<br/>
+			<Typography className="center">
+				404.
+					<br />
 					page not found.
-					<div className="space"/>
-					<Button className="backButton" onClick={goBack}>
-						<span className="buttonText">Let's go back.</span>
-					</Button>
-				</Typography>
-			</Paper>
+					<div className="space" />
+				<Button className="backButton" onClick={goBack}>
+					<span className="buttonText">Let's go back.</span>
+				</Button>
+			</Typography>
 		</div>
 	);
 };
