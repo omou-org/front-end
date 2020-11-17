8625bb0afd3e11abbd95b9a9e94fa9802834644b
Andrew Knapp <ajknapp2015@gmail.com>
2020-11-16 23:19:08 -0800

    Removed background paper from Registration course page

--- src/components/ErrorNotFoundPage/ErrorNotFoundPage.js
+++ src/components/ErrorNotFoundPage/ErrorNotFoundPage.js
@@ -2,7 +2,6 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 
 import Button from "@material-ui/core/Button";
-import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 
 import "./ErrorNotFoundPage.scss";
