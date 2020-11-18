60b5c2299c45cdf4b2f4a768a2f4bda32c042e2e
Peter Kang <peeterkang@gmail.com>
2020-11-17 17:41:11 -0800

    testing checks with this. please ignore

--- src/theme/muiTheme.js
+++ src/theme/muiTheme.js
@@ -1,7 +1,7 @@
 import {createMuiTheme} from "@material-ui/core/styles";
 import createBreakpoints from '@material-ui/core/styles/createBreakpoints'
 
-const breakpoints = createBreakpoints({})
+ const breakpoints = createBreakpoints({})
 
 const tabBorderRadius = "10px";
 // Theme Colors
