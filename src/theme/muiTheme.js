5dae5c5b940dcada670aa5e3da2d490d2907bf07
Andrew Knapp <ajknapp2015@gmail.com>
2020-11-19 11:43:12 -0800

    Merged with DahlDesign

--- src/theme/muiTheme.js
+++ src/theme/muiTheme.js
@@ -112,120 +112,6 @@ const theme = createMuiTheme({
   "palette": {
     "primary": { "main": omouBlue },
     "secondary": { "main": "#a6a6a6" },
-<<<<<<< HEAD
-    "warning": { "main": "#FFDD59" },
-    "background": {
-      "main": neutralBackgroundColor,
-      "default": neutralBackgroundColor,
-    },
-    "success": { "main": "#6CE086" },
-    "warning": { "main": "#FFDD59" },
-    "error": { "main": "#FF6766" },
-  },
-  "colors": {
-    "black": "#000000",
-    "darkGray": "#666666",
-    "darkBlue": "#1F82A1",
-    "omouBlue": "#43B5D9",
-    "skyBlue": "#EBFAFF",
-    "white": "#FFFFFF",
-    "goth": "#000000",
-    "charcoal": "#333333",
-    "slateGrey": "#666666",
-    "gloom": "#999999",
-    "cloudy": "#C4C4C4",
-    "lightGrey": "#D3D3D3",
-    "buttonBlue": "#289FC3",
-    "backgroundGrey": "#EEEEEE",
-    "statusGreen": "#6CE086",
-    "statusYellow": "#FFDD59",
-    "statusRed": "#FF6766",
-  },
-  "typography": {
-    subtitle2: {
-      fontWeight: 500,
-      fontSize: 12,
-    }
-  },
-  "overrides": {
-    "MuiCssBaseline": {
-      "@global": {
-        "body": {
-          "backgroundColor": neutralBackgroundColor,
-        }
-      },
-    },
-    MuiTypography: {
-      variantMapping: {
-        subtitle1: 'h5',
-      },
-      h1: {
-        fontSize: '36px',
-        fontWeight: 'bold',
-        fontFamily: 'Roboto Slab',
-        lineHeight: '48px',
-        color: '#000000'
-      },
-      h2: {
-        fontSize: '32px',
-        fontWeight: 'bold',
-        fontFamily: 'Roboto',
-        lineHeight: '40px',
-        color: '#666666'
-      },
-      h3: {
-        fontSize: '20px',
-        fontWeight: '500',
-        fontFamily: 'Roboto',
-        lineHeight: '32px',
-        color: '#000000'
-      },
-      h4: {
-        fontSize: '16px',
-        fontWeight: '500',
-        fontFamily: 'Roboto',
-        lineHeight: '16px',
-      },
-      h5: {
-        fontSize: '14px',
-        fontWeight: '500',
-        fontStyle: 'normal',
-        fontFamily: 'Roboto',
-        lineHeight: '16px',
-        fontVariant: 'small-caps',
-        fontFeatureSettings: '"cpsp" on'
-      },
-      //Body(Default)
-      body1: {
-        fontSize: '14px',
-        fontWeight: '400',
-        fontFamily: 'Roboto',
-        lineHeight: '16px',
-        color: '#000000'
-      },
-      //Body(Paragraph)
-      h6: {
-        fontSize: '14px',
-        fontWeight: '400',
-        fontFamily: 'Roboto',
-        lineHeight: '22px',
-        color: '#000000'
-      },
-      //Body(Bolded)
-      body2: {
-        fontSize: '14px',
-        fontWeight: '500',
-        fontFamily: 'Roboto',
-        lineHeight: '16px',
-        color: '#000000'
-      },
-      subtitle1: {
-        fontSize: '20px',
-        fontWeight: '300',
-        fontFamily: 'Roboto',
-      },
-    },
-=======
     "warning": { "main": statusYellow },
     "background": {
       "main": backgroundGrey,
@@ -285,7 +171,6 @@ const theme = createMuiTheme({
       body2,
       subtitle1,
     },
->>>>>>> DahlDesign
     "MuiButton": {
       "fontSize": '14px',
       "fontWeight": '500',
@@ -295,11 +180,7 @@ const theme = createMuiTheme({
       "fontVariant": 'all-small-caps',
       "fontFeatureSettings": '"cpsp" on',
       "text": {
-<<<<<<< HEAD
-        "color": "black",
-=======
         "color": goth,
->>>>>>> DahlDesign
       },
       "contained": {
         "fontSize": '14px',
@@ -309,37 +190,23 @@ const theme = createMuiTheme({
         "lineHeight": '16px',
         "fontVariant": 'all-small-caps',
         "fontFeatureSettings": '"cpsp" on',
-<<<<<<< HEAD
-        "color": "#FFFFFF",
-        "backgroundColor": "#289FC3",
-        "border": "2px solid #289FC3",
-=======
         "color": white,
         "backgroundColor": buttonBlue,
         "border": `2px solid ${buttonBlue}`,
->>>>>>> DahlDesign
         "boxSizing": "border-box",
         "borderRadius": "5px",
         "overflow": "hidden",
         "white-space": "nowrap",
         "text-overflow": "ellipsis",
         "&:hover": {
-<<<<<<< HEAD
-          "backgroundColor": "#289FC3",
-=======
           "backgroundColor": buttonBlue,
->>>>>>> DahlDesign
           "opacity": "80%",
           "border": "rgba(40, 159, 195, 0.8)",
           "-webkit-background-clip": "padding-box",
           "background-clip": "padding-box"
         },
         "&:active": {
-<<<<<<< HEAD
-          "backgroundColor": "#289FC3",
-=======
           "backgroundColor": buttonBlue,
->>>>>>> DahlDesign
           "opacity": "60%",
           "border": "rgba(40, 159, 195, 0.6)",
           "-webkit-background-clip": "padding-box",
@@ -354,34 +221,15 @@ const theme = createMuiTheme({
         "lineHeight": '16px',
         "fontVariant": 'all-small-caps',
         "fontFeatureSettings": '"cpsp" on',
-<<<<<<< HEAD
-        "color": "#289FC3",
-        "backgroundColor": "#FFFFFF",
-        "border": "2px solid #C4C4C4",
-=======
         "color": buttonBlue,
         "backgroundColor": white,
         "border": `2px solid ${cloudy}`,
->>>>>>> DahlDesign
         "boxSizing": "border-box",
         "border-radius": "5px",
         "overflow": "hidden",
         "white-space": "nowrap",
         "text-overflow": "ellipsis",
         "&:hover": {
-<<<<<<< HEAD
-          "backgroundColor": "#FFFFFF",
-          "opacity": "90%"
-        },
-        "&:active": {
-          "backgroundColor": "#FFFFFF",
-          "opacity": "70%"
-        },
-        "&:disabled": {
-          "border": "2px solid #999999",
-          "backgroundColor": "#C4C4C4",
-          "color": "#999999",
-=======
           "backgroundColor": white,
           "opacity": "90%"
         },
@@ -393,25 +241,16 @@ const theme = createMuiTheme({
           "border": `2px solid ${gloom}`,
           "backgroundColor": cloudy,
           "color": gloom,
->>>>>>> DahlDesign
           "opacity": "80%"
         },
       },
       containedPrimary: {
-<<<<<<< HEAD
-        color: "#FFFFFF",
-=======
         color: white,
->>>>>>> DahlDesign
       }
     },
     "MuiBadge": {
       "colorPrimary": {
-<<<<<<< HEAD
-        "color": "white",
-=======
         "color": white,
->>>>>>> DahlDesign
         "marginRight": "6px",
         "marginTop": "4px",
       },
@@ -434,20 +273,12 @@ const theme = createMuiTheme({
       },
       "paperAnchorDockedLeft": {
         "borderRight": "none",
-<<<<<<< HEAD
-        backgroundColor: neutralBackgroundColor,
-=======
         backgroundColor: backgroundGrey,
->>>>>>> DahlDesign
         padding: "1vw",
       }
     },
     "paper": {
-<<<<<<< HEAD
-      "background": "#FAFAFA",
-=======
       "background": backgroundGrey,
->>>>>>> DahlDesign
     },
     "MuiMenuItem": {
       "root": {
@@ -463,13 +294,8 @@ const theme = createMuiTheme({
         borderTopLeftRadius: 0,
         borderTopRightRadius: 0,
         marginTop: "0",
-<<<<<<< HEAD
-        color: "#666666",
-        backgroundColor: "#ffffff",
-=======
         color: slateGrey,
         backgroundColor: white,
->>>>>>> DahlDesign
         transition: "0.2s",
         [breakpoints.up("md")]: {
           minWidth: 120
@@ -487,36 +313,6 @@ const theme = createMuiTheme({
             width: 1,
             zIndex: 1,
             marginTop: "0.5em",
-<<<<<<< HEAD
-            backgroundColor: "#EEEEEE"
-          }
-        },
-        "& + $selected:before": {
-          opacity: 0
-        },
-        "&:hover": {
-          "&:not($selected)": {
-            backgroundColor: skyBlue
-          },
-          "&::before": {
-            opacity: 0
-          },
-          "& + $root:before": {
-            opacity: 0
-          }
-        },
-        "&$selected": {
-          "backgroundColor": skyBlue,
-          "color": omouBlue,
-        },
-      },
-      selected: {
-        backgroundColor: skyBlue,
-        color: omouBlue,
-        "& + $root": {
-          zIndex: 1
-        },
-=======
             backgroundColor: backgroundGrey
           }
         },
@@ -545,7 +341,6 @@ const theme = createMuiTheme({
         "& + $root": {
           zIndex: 1
         },
->>>>>>> DahlDesign
         "& + $root:before": {
           opacity: 0
         }
@@ -562,8 +357,7 @@ const theme = createMuiTheme({
     },
     "MuiStepIcon": {
       "text": {
-<<<<<<< HEAD
-        "fill": "white",
+        "fill": white,
       },
     },
     MuiTableHead: {
@@ -587,15 +381,6 @@ const theme = createMuiTheme({
       root: {
         borderBottomColor: cloudy,
         borderBottomWidth: "1px"
-=======
-        "fill": white,
-      },
-    },
-    MuiTableCell: {
-      head: {
-        color: omouBlue,
-        fontWeight: 600,
->>>>>>> DahlDesign
       }
     }
   },
