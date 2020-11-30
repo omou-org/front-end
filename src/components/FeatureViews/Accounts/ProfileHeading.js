7170efa6c62aa6103b372bd47e2b60adabb7ef51
AnnaxGrace <anna.grace.conover@gmail.com>
2020-11-30 11:58:50 -0600

    updated stylings, mutation is returning as 'failed'

--- src/components/FeatureViews/Accounts/ProfileHeading.js
+++ src/components/FeatureViews/Accounts/ProfileHeading.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 import { useSelector } from "react-redux";
 
 import gql from "graphql-tag";
-import {useMutation, useQuery} from "@apollo/react-hooks";
+import { useMutation, useQuery } from "@apollo/react-hooks";
 
 import Button from "@material-ui/core/Button";
 import CalendarIcon from "@material-ui/icons/CalendarToday";
@@ -23,7 +23,7 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 
-import {useSearchParams} from "actions/hooks";
+import { useSearchParams } from "actions/hooks";
 import "./Accounts.scss";
 import { addDashes } from "./accountUtils";
 import { ReactComponent as BirthdayIcon } from "../../birthday.svg";
@@ -40,28 +40,35 @@ import generatePassword from "password-generator";
 import ResetPassword from "components/Authentication/ResetPassword";
 
 const ProfileHeading = ({ user }) => {
-  console.log(user);
   const [anchorEl, setAnchorEl] = useState(null);
   const [open, setOpen] = useState(false);
   const [openReset, setResetOpen] = useState(false);
   const [password, setPassword] = useState();
 
+  //   const RESET_PASSWORD = gql`
+  //     mutation ResetPassword(password: String!, $token: String!, $isParent: Boolean, $isInstructor: Boolean) {
+  //         resetPassword(newPassword: $password, token: $token, setInstructor: $isInstructor, setStudent: $isParent) {
+  //             status
+  //         }
+  // 	}`;
+
   const RESET_PASSWORD = gql`
-    mutation ResetPassword($password: String!, $token: String!, $isParent: Boolean, $isInstructor: Boolean) {
-        resetPassword(newPassword: $password, token: $token, setInstructor: $isInstructor, setParent: $isParent) {
-            status
-        }
-    }`;
+    mutation ResetPassword($password: String!, $token: String!) {
+      resetPassword(newPassword: $password, token: $token) {
+        status
+      }
+    }
+  `;
 
   const params = useSearchParams();
 
   const resetToken = params.get("token");
   console.log("resetToken", resetToken);
 
-  const token = localStorage.getItem("token")
-  console.log("token", token)
+  const token = localStorage.getItem("token");
+  console.log("token", token);
 
-  const [resetPassword, resetStatus] = useMutation(RESET_PASSWORD); 
+  const [resetPassword, resetStatus] = useMutation(RESET_PASSWORD);
 
   const isAdmin =
     useSelector(({ auth }) => auth.accountType) === USER_TYPES.admin;
@@ -90,38 +97,36 @@ const ProfileHeading = ({ user }) => {
   const handleClosePasswordReset = () => {
     setOpen(false);
     setResetOpen(true);
-	setPassword(generatePassword(8, false, /[\w\?\-]/));
-	
-	let isInstructor;
-	let isParent;
-	switch (user.role) {
-		case "instructor":
-			isInstructor = true;
-			isParent = false;
-			break;
-		case "parent":
-			isInstructor = false;
-			isParent = true;
-			break;
-		default:
-			break;
-	}
-	// resetPassword({
-	// 	"variables": {
-	// 		isInstructor,
-	// 		isParent,
-	// 		password: password,
-	// 		"token": token 
-	// 	},
-	// });
+    const newPassword = generatePassword(8, false, /[\w\?\-]/);
+    setPassword(newPassword);
+    console.log(newPassword);
+    console.log(token);
+    console.log(resetToken);
+    // let isInstructor;
+    // let isParent;
+    // switch (user.role) {
+    // 	case "instructor":
+    // 		isInstructor = true;
+    // 		isParent = false;
+    // 		break;
+    // 	case "parent":
+    // 		isInstructor = false;
+    // 		isParent = true;
+    // 		break;
+    // 	default:
+    // 		break;
+    // }
+    resetPassword({
+      variables: {
+        password: newPassword,
+        token: token,
+      },
+    });
   };
 
   const handleCloseReset = () => {
     setResetOpen(false);
-    console.log("do I need");
   };
-
-  console.log(user)
 
   const renderEditandAwayButton = () => (
     <Grid container item xs={4}>
@@ -153,105 +158,30 @@ const ProfileHeading = ({ user }) => {
       {isAdmin && (
         <>
           <Grid component={Hidden} item mdDown xs={12}>
-            <EditIcon
-              className="editIcon" 
-            />
-            <div className = "editResetDiv">
-            <Button
-              className="edit"
-              component={Link}
-              to={`/form/${user.role}/${user.user_id}`}
-            >
-              Edit Profile
-              {/* UPDATE */}
-              {/* Only shows with hover */}
-            </Button>
-			{user.role === "student" ? (
-            <Button
-			  className="resetStudent"
-			  disabled
-              onClick={handleClickOpen}
-            >
-              Reset Password
-            </Button>
-			) : (
-			<Button
-              className="reset"
-              onClick={handleClickOpen}
-            >
-              Reset Password
-            </Button>
-			)}
-            </div>
-            <Dialog
-              open={open}
-              onClose={handleClosePassword}
-              maxWidth="xs"
-              aria-labelledby="alert-dialog-title"
-              aria-describedby="alert-dialog-description"
-            >
-              <DialogTitle id="alert-dialog-title" className="center dialog-padding">
-                {"Do you want to reset this user's password?"}
-              </DialogTitle>
-              <DialogContent>
-                <DialogContentText id="alert-dialog-description"className ="center">
-                  When you reset the password, a randomly generated password
-                  will become the user's new password. This action cannot be
-                  reverted. An automatic email will be sent out notifying the
-                  user that their password has been reset. The user can also
-                  reset their own password through the portal login page.
-                </DialogContentText>
-              </DialogContent>
-              <DialogActions
-			  	className = "dialog-actions"
-			  >
-				<Button 
-				onClick={handleClosePassword} 
-				variant="outlined">
-                  cancel
-                </Button>
+            <EditIcon className="editIcon" />
+            <div className="editResetDiv">
+              <Button
+                className="edit"
+                component={Link}
+                to={`/form/${user.role}/${user.user_id}`}
+              >
+                Edit Profile
+              </Button>
+              {user.role === "student" ? (
                 <Button
-                  onClick={handleClosePasswordReset}
-                  className="resetBtn"
-                  autoFocus
+                  className="resetStudent"
+                  disabled
+                  onClick={handleClickOpen}
                 >
-                  reset password
+                  Reset Password
                 </Button>
-              </DialogActions>
-            </Dialog>
-            <Dialog
-              open={openReset}
-              onClose={handleCloseReset}
-              aria-labelledby="alert-dialog-title"
-              aria-describedby="alert-dialog-description"
-            >
-              <DialogTitle id="alert-dialog-title" className="center">
-                {"Password has been reset."}
-              </DialogTitle>
-              <DialogContent className = "center dialog-padding">
-                <DialogContentText id="alert-dialog-description">
-                  The new password for {user.first_name} {user.last_name} ID #
-                  {user.user_id} is
-                </DialogContentText>
-                <DialogContentText id="alert-dialog-description" className="passwordDisplay">
-                  {password}
-                </DialogContentText>
-                <DialogContentText id="alert-dialog-description">
-                  Please write it down since you will not be able to access it
-                  later
-                </DialogContentText>
-              </DialogContent>
-              <DialogActions
-			  className="dialog-actions"
-			  >
-				<Button 
-				onClick={handleCloseReset}
-				color="primary"
-				>
-                  done
+              ) : (
+                <Button className="reset" onClick={handleClickOpen}>
+                  Reset Password
                 </Button>
-              </DialogActions>
-            </Dialog>
+              )}
+            </div>
+            <ResetDialog />
           </Grid>
           <Grid component={Hidden} item lgUp xs={12}>
             <Button
@@ -263,6 +193,73 @@ const ProfileHeading = ({ user }) => {
         </>
       )}
     </Grid>
+  );
+
+  const ResetDialog = () => (
+    <>
+      <Dialog
+        open={open}
+        onClose={handleClosePassword}
+        maxWidth="xs"
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title" className="center dialog-padding">
+          {"Do you want to reset this user's password?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description" className="center">
+            When you reset the password, a randomly generated password will
+            become the user's new password. This action cannot be reverted. An
+            automatic email will be sent out notifying the user that their
+            password has been reset. The user can also reset their own password
+            through the portal login page.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions className="dialog-actions">
+          <Button onClick={handleClosePassword} variant="outlined" className="cancelBtn">
+            cancel
+          </Button>
+          <Button
+            onClick={handleClosePasswordReset}
+            className="resetBtn"
+            autoFocus
+          >
+            reset password
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <Dialog
+        open={openReset}
+        onClose={handleCloseReset}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title" className="center">
+          {"Password has been reset."}
+        </DialogTitle>
+        <DialogContent className="center dialog-padding">
+          <DialogContentText id="alert-dialog-description">
+            The new password for {user.first_name} {user.last_name} ID #
+            {user.user_id} is
+          </DialogContentText>
+          <DialogContentText
+            id="alert-dialog-description"
+            className="passwordDisplay"
+          >
+            {password}
+          </DialogContentText>
+          <DialogContentText id="alert-dialog-description">
+            Please write it down since you will not be able to access it later
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions className="dialog-actions">
+          <Button onClick={handleCloseReset} className = "doneBtn">
+            done
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 
   const profileDetails = useMemo(() => {
@@ -279,7 +276,6 @@ const ProfileHeading = ({ user }) => {
       </>
     );
 
-    //set up dialog like these
     const EmailRow = () => (
       <>
         <Grid className="emailPadding" item md={1}>
