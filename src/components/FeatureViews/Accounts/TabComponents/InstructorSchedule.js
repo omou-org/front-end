dc3d2ac2b6bdc9c1fc9b845be117f24727fbb53b
Andrew Knapp <ajknapp2015@gmail.com>
2020-11-30 21:33:03 -0800

    Finished InstructorSchedule gql migration (except for allDay)

--- src/components/FeatureViews/Accounts/TabComponents/InstructorSchedule.js
+++ src/components/FeatureViews/Accounts/TabComponents/InstructorSchedule.js
@@ -1,18 +1,14 @@
-import React, {useMemo} from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import {useSelector} from "react-redux";
 
 import FullCalendar from "@fullcalendar/react";
 import timeGridPlugin from "@fullcalendar/timegrid";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
 
-import * as hooks from "actions/hooks";
 import {handleToolTip} from "../../Scheduler/SchedulerUtils";
 import { fullName } from "utils.js";
-import Loading from "components/OmouComponents/Loading";
 import {stringToColor} from "../accountUtils";
-import gql from "graphql-tag";
-import { useQuery } from "@apollo/react-hooks";
-import { Redirect } from "react-router-dom";
 
 const GET_INSTRUCTOR_INFO = gql`
 	query getCourses($instructorID: ID!) {
@@ -35,6 +31,11 @@ const GET_INSTRUCTOR_INFO = gql`
 				  startTime
 				  endTime
 				}
+				instructoroutofofficeSet {
+					endDatetime
+					startDatetime
+					description
+				  }
 			  }
 			}
 		  }
@@ -44,106 +45,7 @@ const GET_INSTRUCTOR_INFO = gql`
 const toHours = (ms) => ms / 1000 / 60 / 60;
 
 const InstructorSchedule = ({instructorID}) => {
-	// const sessions = useSelector(({Calendar}) => Calendar.CourseSessions);
-	// const instructor = useSelector(
-	// 	({Users}) => Users.InstructorList[instructorID]
-	// );
-	// const OOOstatus = hooks.useOutOfOffice();
-	// const availabilityStatus = hooks.useInstructorAvailability(instructorID);
-	// const classEnrollmentStatus = hooks.useClassSessionsInPeriod("week");
-	// const tutoringEnrollmentStatus = hooks.useTutoringSessionsInPeriod("week");
 
-	// const teachingSessions = useMemo(
-	// 	() =>
-	// 		Object.values(sessions[instructorID] || {}).map((session) => ({
-	// 			end: new Date(session.end_datetime),
-	// 			start: new Date(session.start_datetime),
-	// 			title: session.title,
-	// 		})),
-	// 	[sessions, instructorID]
-	// );
-
-	// const OOO = useMemo(
-	// 	() =>
-	// 		instructor
-	// 			? Object.values(instructor.schedule.time_off).map(
-	// 			({all_day, description, start, end}) => {
-	// 				const endDate = new Date(end);
-	// 				// since the end date for allDay events is EXCLUSIVE
-	// 				// must add one day to include the end specified by user
-	// 				if (all_day) {
-	// 					endDate.setDate(endDate.getDate() + 1);
-	// 				}
-	// 				return {
-	// 					allDay: all_day,
-	// 					end: endDate,
-	// 					start,
-	// 					title: description || "Out of Office",
-	// 				};
-	// 			}
-	// 			)
-	// 			: [],
-	// 	[instructor]
-	// );
-
-	// const allDayOOO = useMemo(() => OOO.some(({allDay}) => allDay), [OOO]);
-
-	// const hoursWorked = useMemo(
-	// 	() =>
-	// 		Math.round(
-	// 			Object.values(sessions[instructorID] || []).reduce((hours, session) => {
-	// 				const start = new Date(session.start_datetime);
-	// 				const end = new Date(session.end_datetime);
-	// 				if (start > Date.now()) {
-	// 					return hours;
-	// 				} else if (end < Date.now()) {
-	// 					return hours + toHours(end - start);
-	// 				}
-	// 				return hours + toHours(end - Date.now());
-	// 			}, 0) * 100
-	// 		) / 100,
-	// 	[sessions, instructorID]
-	// );
-
-	// const instructorBusinessHours = useMemo(
-	// 	() =>
-	// 		instructor
-	// 			? Object.values(instructor.schedule.work_hours).map(
-	// 			({day, start, end}) => ({
-	// 				daysOfWeek: [day],
-	// 				endTime: end,
-	// 				startTime: start,
-	// 			})
-	// 			)
-	// 			: [],
-	// 	[instructor]
-	// );
-
-	// if (
-	// 	teachingSessions.length === 0 &&
-	// 	instructorBusinessHours.length === 0 &&
-	// 	OOO.length === 0
-	// ) {
-	// 	if (
-	// 		hooks.isLoading(
-	// 			tutoringEnrollmentStatus,
-	// 			OOOstatus,
-	// 			classEnrollmentStatus,
-	// 			availabilityStatus
-	// 		)
-	// 	) {
-	// 		return <Loading/>;
-	// 	} else if (
-	// 		hooks.isFail(
-	// 			tutoringEnrollmentStatus,
-	// 			OOOstatus,
-	// 			classEnrollmentStatus,
-	// 			availabilityStatus
-	// 		)
-	// 	) {
-	// 		return "Unable to load schedule!";
-	// 	}
-	// }
 
 	const { loading, error, data } = useQuery(GET_INSTRUCTOR_INFO, {
 		variables: {instructorID}
@@ -151,46 +53,51 @@ const InstructorSchedule = ({instructorID}) => {
 
 	if (loading ) return null;
 
-	if (error ) return <Redirect to="/PageNotFound" />;
-
-	console.log(data);
+	if (error ) return "Unable to load schedule";
 
 	const { user } = data.instructor;
 
-	const hoursWorked = parseInt(user.instructor.sessionSet.reduce((hours, session) => {
+	const today = new Date(Date.now());
+
+	const hoursWorkedThisMonth = parseInt(user.instructor.sessionSet.reduce((hours, session) => {
 		const start = new Date(session.startDatetime);
 		const end = new Date(session.endDatetime);
-		console.log(toHours(end - start));
-		if (start > Date.now()) {
-			return hours;
-		} else if (end < Date.now()) {
-			return hours + toHours(end - start);
-		}
-		return hours + toHours(end - Date.now());
-	}, 0));
 
-	const teachingSessions = user.instructor.sessionSet.map(({endDatetime, startDatetime, title}) => ({
-				end: new Date(endDatetime),
-				start: new Date(startDatetime),
-				title: title,
-			}));
+		if (start.getMonth() === today.getMonth() &&
+			start.getFullYear() === today.getFullYear()) {
+			return hours + toHours(end - start);
+		} else {
+			return hours;
+		}
+
+	}, 0));
 
 	const instructorBusinessHours = user.instructor.instructoravailabilitySet.map(({dayOfWeek, startTime, endTime}) => ({
 		dayOfWeek: [dayOfWeek],
 		startTime, 
 		endTime
 	}))
-	
+
+	const teachingSessions = user.instructor.sessionSet.map(({endDatetime, startDatetime, title}) => ({
+		title: title,
+		end: new Date(endDatetime),
+		start: new Date(startDatetime),
+	}));
+
+	// Out Of Office
+	const OOO = user.instructor.instructoroutofofficeSet.map(({startDatetime, endDatetime, description}) => ({
+		title: description,
+		start: new Date(startDatetime),
+		end: new Date(endDatetime)
+	}));
 
 	console.log(teachingSessions);
-	//let hoursWorked = 0;
-	let OOO = [];
-	let allDayOOO = true;
+	const allDayOOO = true;
 	
 	return (
 		<>
 			<h3 style={{float: "left"}}>
-				{hoursWorked} hour{hoursWorked !== 1 && "s"} worked this month
+				{hoursWorkedThisMonth} hour{hoursWorkedThisMonth !== 1 && "s"} worked this month
 			</h3>
 			<FullCalendar
 				allDaySlot={allDayOOO}
