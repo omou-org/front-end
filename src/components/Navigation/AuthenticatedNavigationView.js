b6bf068c41e25316b3c1e0cdf6580c79371a26f0
Michael Li <ml5885@pleasantonusd.net>
2020-12-12 23:10:51 -0800

    Moved google sign in popup code from Navigation.js to AuthenticatedNavigationView.js

--- src/components/Navigation/AuthenticatedNavigationView.js
+++ src/components/Navigation/AuthenticatedNavigationView.js
@@ -1,4 +1,5 @@
 import React, {useCallback, useState} from "react";
+import {useSelector, useDispatch} from "react-redux";
 import Hidden from "@material-ui/core/Hidden";
 import Drawer from "@material-ui/core/Drawer";
 import AuthenticatedNavBar from "./AuthenticatedNavBar";
@@ -12,6 +13,15 @@ import {useQuery} from "@apollo/react-hooks";
 import Loading from "../OmouComponents/Loading";
 import OnboardingRoutes from "../Routes/OnboardingRoutes";
 import IdleTimerPrompt from "../OmouComponents/IdleTimerPrompt";
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import {GoogleLogin, GoogleLogout} from "react-google-login";
+import axios from "axios"; 
+import * as actions from "actions/actionTypes";
 
 const useStyles = makeStyles({
 	navigationIconStyle: {
@@ -32,13 +42,40 @@ const CHECK_BUSINESS_EXISTS = gql`
 `
 
 export default function AuthenticatedNavigationView({UserNavigationOptions}) {
+	const dispatch = useDispatch();
+
 	const classes = useStyles();
 	const [mobileOpen, setMobileOpen] = useState(false);
 	const {data, loading, error} = useQuery(CHECK_BUSINESS_EXISTS);
 
+	const [googleLoginPromptOpen, setGoogleLoginPromptOpen] = useState(false);
+	const [response, setResponse] = useState();
+	const { email } = useSelector(({ auth }) => auth);
+
+
 	const handleDrawerToggle = useCallback(() => {
 		setMobileOpen((open) => !open);
 	}, []);
+
+	const responseGoogle = (response) => {
+        console.log(response);
+	}
+	
+	const onSuccess = (status) => {
+        setResponse(status);
+        console.log("Success");
+        dispatch({
+            type: actions.SET_GOOGLE_TOKEN, 
+            payload: {google_access_token: response.tokenObj.access_token}
+        })
+        setGoogleLoginPromptOpen(false);
+    };
+
+    useEffect(() => {
+        if(email !== null){
+            setGoogleLoginPromptOpen(true);
+        }
+    }, [email]);
 
 	if (loading) return <Loading/>
 	if (error) return <div>There's been an error! {error.message}</div>
@@ -71,6 +108,35 @@ export default function AuthenticatedNavigationView({UserNavigationOptions}) {
 						<RootRoutes/>
 					</main>
 				</MuiPickersUtilsProvider>
+				<div>
+				<Dialog
+					open={googleLoginPromptOpen}
+					onClose={handleClose}
+					aria-labelledby="dialog-title"
+					aria-describedby="dialog-description"
+				>
+					<DialogTitle id="dialog-title">{"Sign in with Google"}</DialogTitle>
+					<DialogContent>
+					<DialogContentText id="dialog-description">
+						Allow us to access your Google Classroom courses
+					</DialogContentText>
+					</DialogContent>
+					<DialogActions>
+						<Button onClick={handleClose} color="primary">
+							No
+						</Button>
+						
+						<GoogleLogin
+						buttonText="Login"
+						clientId="1059849289788-0tpge112i2bfe5llak523fdopu8foul7.apps.googleusercontent.com"
+						isSignedIn
+						onFailure={responseGoogle}
+						onSuccess={onSuccess}
+						scope = "https://www.googleapis.com/auth/classroom.courses https://www.googleapis.com/auth/classroom.coursework.me.readonly https://www.googleapis.com/auth/classroom.profile.emails https://www.googleapis.com/auth/classroom.profile.photos https://www.googleapis.com/auth/classroom.rosters "
+						/> 
+					</DialogActions>
+				</Dialog>
+			</div>
 			</div> : <OnboardingRoutes/>
 		}
 		<IdleTimerPrompt/>
