f9f17633b76c92b48ae0aa672eb48a037c625bde
CalvinFronda <CalvinFronda@users.noreply.github.com>
2021-02-02 23:14:34 +0000

    Apply formatting changes

--- src/components/OmouComponents/AddItemButton.js
+++ src/components/OmouComponents/AddItemButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import { PropTypes } from 'prop-types';
-import {defaultBoxShadow} from '../../theme/muiTheme'
+import { defaultBoxShadow } from '../../theme/muiTheme';
 
 const useStyles = makeStyles({
     addNewItemStyles: ({ height, width }) => ({
