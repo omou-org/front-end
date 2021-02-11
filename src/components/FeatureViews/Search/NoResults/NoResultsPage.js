aeb87f43eac7e6bc3a3cd89c7a9a9379a0fdc94b
CalvinFronda <CalvinFronda@users.noreply.github.com>
2021-02-11 00:56:21 +0000

    Apply formatting changes

--- src/components/FeatureViews/Search/NoResults/NoResultsPage.js
+++ src/components/FeatureViews/Search/NoResults/NoResultsPage.js
@@ -20,8 +20,8 @@ const useStyles = makeStyles({
         textAlign: 'left',
     },
     suggestionHeader: {
-        paddingTop: "4vh",
-        paddingBottom: "2vh",
+        paddingTop: '4vh',
+        paddingBottom: '2vh',
         fontSize: '32px',
         fontWeight: '500',
     },
@@ -41,74 +41,74 @@ const NoResultsPage = () => {
                 <BackButton />
                 <hr />
 
-                <Grid container direction="column"
-                justify="center"
-                alignItems="center"
-                spacing={5}
+                <Grid
+                    container
+                    direction='column'
+                    justify='center'
+                    alignItems='center'
+                    spacing={5}
                 >
-
-                <Grid item xs={12} className={classes.center}>
-                    <Typography
-                        variant='h1'
-                        style={{ paddingBottom: '20px' }}
-                        data-cy='no-results-header'
-                    >
-                        Sorry, no results were found
-                    </Typography>
-
-                    <Grid item xs={12} >
-                        <Typography className={classes.suggestionHeader}>
-                            Suggestions 
+                    <Grid item xs={12} className={classes.center}>
+                        <Typography
+                            variant='h1'
+                            style={{ paddingBottom: '20px' }}
+                            data-cy='no-results-header'
+                        >
+                            Sorry, no results were found
                         </Typography>
+
+                        <Grid item xs={12}>
+                            <Typography className={classes.suggestionHeader}>
+                                Suggestions
+                            </Typography>
                         </Grid>
                         <Grid>
-                        <Typography variant='subtitle1' style={{paddingTop: "2vh"}}>
-                            Check your spelling
-                            <br />
-                            
-                            Try more general words
-                        </Typography>
-                        <Grid item xs={12}>
-                        <Typography className={classes.popularHeader}>
-                            Popular
-                        </Typography>
-                        </Grid>
-
-                        <Grid item xs={12} >
-                        <Typography variant='subtitle1'>
-                            <Link
-                                href='/scheduler'
-                                data-cy='schedulerLink'
-                                style={{
-                                    fontSize: "20px",
-                                    borderBottom: '2px solid currentColor',
-                                }}
+                            <Typography
+                                variant='subtitle1'
+                                style={{ paddingTop: '2vh' }}
                             >
-                                Scheduler
-                            </Link>
+                                Check your spelling
+                                <br />
+                                Try more general words
                             </Typography>
+                            <Grid item xs={12}>
+                                <Typography className={classes.popularHeader}>
+                                    Popular
+                                </Typography>
                             </Grid>
 
+                            <Grid item xs={12}>
+                                <Typography variant='subtitle1'>
+                                    <Link
+                                        href='/scheduler'
+                                        data-cy='schedulerLink'
+                                        style={{
+                                            fontSize: '20px',
+                                            borderBottom:
+                                                '2px solid currentColor',
+                                        }}
+                                    >
+                                        Scheduler
+                                    </Link>
+                                </Typography>
+                            </Grid>
 
-                        <Grid item xs={12} >
-                        <Typography variant='subtitle1'>
-                    
-                        <Link
-                                href={'/registration'}
-                                style={{
-                                    fontSize: "20px",
-                                    borderBottom: '2px solid currentColor',
-                                }}
-                            >
-                                Registration Catalogue
-                            </Link>
-                            
-                     
-                        </Typography>
-
+                            <Grid item xs={12}>
+                                <Typography variant='subtitle1'>
+                                    <Link
+                                        href={'/registration'}
+                                        style={{
+                                            fontSize: '20px',
+                                            borderBottom:
+                                                '2px solid currentColor',
+                                        }}
+                                    >
+                                        Registration Catalogue
+                                    </Link>
+                                </Typography>
+                            </Grid>
+                        </Grid>
                     </Grid>
-                    </Grid>
-                </Grid>
                 </Grid>
             </Grid>
         </Grid>
