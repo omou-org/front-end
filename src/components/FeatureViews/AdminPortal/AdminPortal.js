2e1cc1126e48f74071df0ec24801c2f58bf7f86a
Peter Kang <peeterkang@gmail.com>
2021-04-01 13:11:38 -0700

    add Bulk Upload button to Admin Portal
    add Bulk Upload button positioned on right side of Admin Portal view per Figma

--- src/components/FeatureViews/AdminPortal/AdminPortal.js
+++ src/components/FeatureViews/AdminPortal/AdminPortal.js
@@ -6,18 +6,20 @@ import Typography from '@material-ui/core/Typography';
 import './AdminPortal.scss';
 import AdminActionCenter from './AdminActionCenter';
 import AdminViewsRoutes from 'components/Routes/AdminViewsRoutes';
-import BackButton from 'components/OmouComponents/BackButton';
-import Box from '@material-ui/core/Box';
+import { ResponsiveButton } from '../../../theme/ThemedComponents/Button/ResponsiveButton'
 
 const AdminPortal = () => (
     <form>
-        <Grid container layout='row'>
-            <Grid item xs={12}>
-                <Box paddingTop='16px'>
+        <Grid container direction='row'>
+            <Grid container direction='row'>
+                <Grid item xs={6}>
                     <Typography align='left' variant='h1'>
                         Admin Portal
                     </Typography>
-                </Box>
+                </Grid>
+                <Grid item xs={6} style={{ textAlign:'right'}}>
+                    <ResponsiveButton variant='outlined' >Bulk Upload</ResponsiveButton>
+                </Grid>
             </Grid>
             <Grid item xs={12}>
                 <AdminActionCenter />
