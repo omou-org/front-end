6706ce97e43f2a4f19d2749313bcb7258d783603
Daniel Huang <danielhuang@g.ucla.edu>
2021-04-19 20:35:43 -0700

    enhancement(): dark highlight on non-upcoming sessions on EnrollmentSessionRow.js

--- src/components/FeatureViews/Enrollment/EnrollmentSessionRow.js
+++ src/components/FeatureViews/Enrollment/EnrollmentSessionRow.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
-import Moment from 'react-moment';
 import SessionPaymentStatusChip from 'components/OmouComponents/SessionPaymentStatusChip';
 import moment from 'moment';
+import Moment from 'react-moment';
+import {makeStyles} from "@material-ui/core/styles";
 
-function EnrollmentSessionRow({ session, enrollmentData, highlightSession }) {
-    const { course, id } = enrollmentData.enrollment;
+const useStyles = makeStyles((theme) => ({
+    darkRow: {
+        backgroundColor: "#CECED1",
+    },
+}));
+
+function EnrollmentSessionRow({session, enrollmentData, highlightSession}) {
+    const classes = useStyles();
+    const {course, id} = enrollmentData.enrollment;
     const tuitionStartTime = moment(session.startDatetime).format('hh');
     const tuitionEndTime = moment(session.endDatetime).format('hh');
     const tuition =
         session.course.hourlyTuition * (tuitionEndTime - tuitionStartTime);
+    const today = moment(new Date());
+    const sessionDate = moment(session.startDatetime);
+    const sessionDaysFromToday = sessionDate.diff(today, 'days');
+    const isUpcomingSession = 7 > sessionDaysFromToday && sessionDaysFromToday > 0;
+    const isNotHighlightSession = highlightSession && !isUpcomingSession;
 
     return (
         <Grid
@@ -26,7 +39,7 @@ function EnrollmentSessionRow({ session, enrollmentData, highlightSession }) {
         >
             <Paper
                 className={`session-info
-                    ${highlightSession && ' active'}
+                    ${isNotHighlightSession && classes.darkRow}
                     ${session.id == session.id && 'upcoming-session'}
                  `}
                 component={Grid}
