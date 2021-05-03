89eccb5a423bf1c992e578a86538e30aea0dc852
Daniel Huang <danielhuang@g.ucla.edu>
2021-05-02 19:42:45 -0700

    fix(linter): cleaned up linter errors

--- src/components/FeatureViews/Onboarding/OnboardingContext.js
+++ src/components/FeatureViews/Onboarding/OnboardingContext.js
@@ -1,21 +1,21 @@
-import React, { useReducer } from 'react';
+import React from 'react';
 
 const onboardingContext = {};
 
 
 export const initalState = {
     UPLOAD_RESPONSE: null,
-}
+};
 
 
 export const reducer = (state, action) => {
     switch (action.type) {
         case 'UPLOAD_RESPONSE':
-            return { ...state, UPLOAD_RESPONSE: action.payload }
+            return {...state, UPLOAD_RESPONSE: action.payload};
 
         default:
             return new Error();
     }
-}
+};
 
 export const OnboardingContext = React.createContext(onboardingContext);
