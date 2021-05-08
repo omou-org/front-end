741e29efdbae3b1a5f4807300c8351ae4459175b
Andrew Knapp <ajknapp2015@gmail.com>
2021-05-07 21:39:38 -0700

    feat (Stripe Integration) Added Powered by Stripe component

--- src/components/OmouComponents/3rdPartyCustomComponents/StripeIcon.js
+++ src/components/OmouComponents/3rdPartyCustomComponents/StripeIcon.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import StripeLogo from './Stripe.png';
+import StripeLogo from './StripeLogo.svg';
 
 const StripeIcon = () => {
-    return <img src={StripeLogo} style={{padding: '5px 5px 0px 0px'}}/>
+    return <img src={StripeLogo} style={{padding: '5px 5px 0px 0px'}} alt="Stripe S Logo"/>
 }
 
 export default StripeIcon;
