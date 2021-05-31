66a4f1b8ad8d0bd923da6bfb4c5090db8f8efeec
Andrew Knapp <ajknapp2015@gmail.com>
2021-05-30 18:14:03 -0700

    Added stripe onboarding url to connect stripe button

--- src/components/OmouComponents/3rdPartyCustomComponents/StripeButton.js
+++ src/components/OmouComponents/3rdPartyCustomComponents/StripeButton.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Grid } from '@material-ui/core';
 import { ResponsiveButton } from 'theme/ThemedComponents/Button/ResponsiveButton';
 import StripeIcon from './StripeIcon'
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
 
+const INITIATE_STRIPE_ONBOARDING = gql`
+    mutation InitiateStripeOnboarding {
+        stripeOnboarding {
+          onboardingUrl
+        }
+      }
+`
 
 const StripeButton = () => {
+
+    const [stripeOnboardingLoading, setStripeOnboardingLoading] = useState(false);
+
+    const [InitiateStripeOnboarding] = useMutation(INITIATE_STRIPE_ONBOARDING, {
+        onCompleted: ( {stripeOnboarding: { onboardingUrl }} ) => {
+            setStripeOnboardingLoading(false);
+            window.open(onboardingUrl, '_blank');
+        },
+        onError: () => {
+            console.log('OOps, there has been an error connecting to stripe')
+        },
+    });
+
     return (
     <>
     <ResponsiveButton 
@@ -15,6 +37,11 @@ const StripeButton = () => {
                     paddingRight: '5px'
                 }}
                 startIcon={StripeIcon}
+                onClick={() => {
+                    setStripeOnboardingLoading(true);
+                    InitiateStripeOnboarding();
+                }}
+                disabled={stripeOnboardingLoading}
     >
         <Grid container justify='flex-start' alignItems='center'>
             <Grid item >
@@ -24,7 +51,7 @@ const StripeButton = () => {
                 <p>CONNECT STRIPE</p>
             </Grid>
         </Grid>
-        </ResponsiveButton>
+    </ResponsiveButton>
     </>)
 }
 
