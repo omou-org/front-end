aee46d244c606ec7d0b5940eba3e2ae6124ed8c8
Andrew Knapp <ajknapp2015@gmail.com>
2021-06-09 20:10:27 -0700

    Feat(Update Invoice): Added Save button to Update Invoice"

--- src/components/FeatureViews/Invoices/UpdateInvoice.js
+++ src/components/FeatureViews/Invoices/UpdateInvoice.js
@@ -6,9 +6,16 @@ import { GET_PAYMENT } from './InvoiceReceipt';
 import Loading from 'components/OmouComponents/Loading';
 import { fullName } from 'utils';
 import Box from '@material-ui/core/Box';
+import { ResponsiveButton } from 'theme/ThemedComponents/Button/ResponsiveButton';
 
 import UpdateInvoiceRow from './UpdateInvoiceRow';
 import { makeStyles } from '@material-ui/core/styles';
+
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
 
 const useStyles = makeStyles({
     heading: {
@@ -17,6 +24,12 @@ const useStyles = makeStyles({
     },
     studentName: {
         marginBottom: '24px'
+    },
+    paymentFormHeading: {
+        textAlign: 'left',
+    },
+    paymentForm: {
+        marginTop: '20px'
     }
 });
 
@@ -24,6 +37,7 @@ const UpdateInvoice = () => {
     const { invoiceId } = useParams();
     const [registrationsByStudent, setRegistrationsByStudent] = useState([]);
     const [registrationsToBeCancelled, setRegistrationsToBeCancelled] = useState([]);
+    const [unsavedChanges, setUnsavedChanges] = useState(false);
 
     const classes = useStyles();
 
@@ -57,6 +71,12 @@ const UpdateInvoice = () => {
         setRegistrationsByStudent(registrationData);
     }, [ data ])    
 
+    // TODO 
+    // [x] Grey out enrollment 
+    // [] Update invoice total using priceQuote query
+    // [x] change x to +
+    // [x] Disable/ grey out Pay Now button
+    // [x] display save button
     const updateCancelledRegistrations = (registrationId) => {
 
         const indexToDelete = registrationsToBeCancelled.indexOf(registrationId);
@@ -70,7 +90,30 @@ const UpdateInvoice = () => {
         }
 
         setRegistrationsToBeCancelled(newRegistrationsToBeCancelledState);
+        setUnsavedChanges(true);
     }
+
+    // TODO
+    // [] Display Save Popup
+    const handleSaveClick = () => {
+
+    }
+
+    
+const paymentOptions = [
+    {
+        label: 'Credit Card',
+        value: 'credit_card',
+    },
+    {
+        label: 'Cash',
+        value: 'cash',
+    },
+    {
+        label: 'Check',
+        value: 'check',
+    },
+];
 
     if (loading) {
         return <Loading />;
@@ -88,6 +131,7 @@ const UpdateInvoice = () => {
           direction='column'
           justify='flex-start'
           alignItems='flex-start'
+          
         >
             <Grid item className={classes.heading}>
                 <Typography variant='h1' >
@@ -115,6 +159,59 @@ const UpdateInvoice = () => {
                 )
             })}
             </Box>
+            <Grid container className={classes.paymentForm} justify='space-between'>
+                <Grid item>
+                    <FormControl component='fieldset'>
+                        <FormLabel component='legend' className={classes.paymentFormHeading}>
+                            <Typography
+                                align='left'
+                                style={{ fontWeight: '600' }}
+                            >
+                                Payment Method
+                            </Typography>
+                        </FormLabel>
+                        <RadioGroup
+                            aria-label='gender'
+                            name='gender1'
+                            row
+                            // onChange={handleMethodChange}
+                            // value={paymentMethod}
+                        >
+                            {paymentOptions.map(({ label, value }) => (
+                                <FormControlLabel
+                                    color='primary'
+                                    control={<Radio />}
+                                    data-cy={`${label}-checkbox`}
+                                    key={value}
+                                    label={label}
+                                    value={value}
+                                />
+                            ))}
+                        </RadioGroup>
+                    </FormControl>
+                </Grid>
+                <Grid item>
+                    <Grid item container direction='row' alignItems='center'>
+                        <Typography variant='body'>Subtotal</Typography>
+                        <Typography variant='body2'>$ the monies</Typography>
+                    </Grid>
+                    <Grid item container direction='row' alignItems='center'>
+                        <Typography variant='body'>Total</Typography>
+                        <Typography variant='body2'>$ the monies</Typography>
+                    </Grid>
+                </Grid>    
+            </Grid>
+            <Grid container justify='flex-end' spacing='3'>
+                <Grid item>
+                    <ResponsiveButton variant='outlined'>Drop Invoice</ResponsiveButton>
+                </Grid>
+                <Grid item>
+                    <ResponsiveButton variant={unsavedChanges ? 'outlined' : 'disabled'}>Save</ResponsiveButton>
+                </Grid>
+                <Grid item>
+                    <ResponsiveButton variant={unsavedChanges ? 'disabled' : 'contained'}>Pay Now</ResponsiveButton>
+                </Grid>
+            </Grid>
         </Grid>
     )
 }
